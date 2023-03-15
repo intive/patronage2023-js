1dd1052837cb3e66b1479b1b6071c98ef8e1d046
Bartosz Kędziora <bartosz.kedziora1@gmail.com>
2023-03-15 19:15:43 +0100

    feat: initial setup for icon component #P23-119

---
+++ apps/storybook/stories/Icon.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Icon } from "ui";
+
+export default {
+  title: "Icon",
+  component: Icon,
+} as ComponentMeta<typeof Icon>;
+
+const Template: ComponentStory<typeof Icon> = ({ ...args }) => (
+  <Icon {...args} />
+);
+
+export const Normal = Template.bind({});
+Normal.args = {
+  children: "Normal",
+};
---
+++ packages/ui/Icon/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Icon = () => {
+    return (
+        <div>
+Test
+        </div>
+    );
+};
