09950193a576129452624705400a1bf2e2e9e353
Daniel Chmur <daniel.chmur@gmail.com>
2023-03-15 11:00:08 +0100

    feat: buttons #P23-109

--- apps/storybook/stories/Button.stories.tsx
+++ apps/storybook/stories/Button.stories.tsx
@@ -6,25 +6,65 @@ import { Button } from "ui";
 export default {
   title: "Button",
   component: Button,
+  argTypes: {
+    secondary: { control: "boolean" },
+    simple: { control: "boolean" },
+    fullWidth: { control: "boolean" },
+  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = ({ children, ...args }) => (
   <Button {...args}>{children}</Button>
 );
 
-export const Normal = Template.bind({});
-Normal.args = {
-  children: "Normal",
-};
-
-export const Disabled = Template.bind({});
-Disabled.args = {
-  children: "Disabled",
-  disabled: true,
+export const Primary = Template.bind({});
+Primary.args = {
+  children: "Primary",
+  disabled: false,
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   children: "Secondary",
   secondary: true,
+};
+
+export const Simple = Template.bind({});
+Simple.args = {
+  children: "Simple",
+  simple: true,
+};
+
+export const NormalFullWidth = Template.bind({});
+NormalFullWidth.args = {
+  children: "Normal FullWidth",
+  fullWidth: true,
+};
+
+export const SecondaryFullWidth = Template.bind({});
+SecondaryFullWidth.args = {
+  children: "Secondary FullWidth",
+  secondary: true,
+  fullWidth: true,
+};
+
+export const PrimaryDisabled = Template.bind({});
+PrimaryDisabled.args = {
+  children: "Primary Disabled",
+  secondary: false,
+  disabled: true,
+};
+
+export const SecondaryDisabled = Template.bind({});
+SecondaryDisabled.args = {
+  children: "Secondary Disabled",
+  secondary: true,
+  disabled: true,
+};
+
+export const SimpleDisabled = Template.bind({});
+SimpleDisabled.args = {
+  children: "Simple Disabled",
+  simple: true,
+  disabled: true,
 };
--- packages/ui/Button/index.tsx
+++ packages/ui/Button/index.tsx
@@ -4,53 +4,99 @@ import * as React from "react";
 import styled, { css } from "styled-components";
 
 export const Button = ({
-  disabled = false,
   secondary = false,
+  simple = false,
+  fullWidth,
+  disabled,
   children,
   onClick,
 }: ButtonProps) => {
   return (
-    <ButtonStyled disabled={disabled} secondary={secondary} onClick={onClick}>
+    <ButtonStyled
+      secondary={secondary}
+      simple={simple}
+      onClick={onClick}
+      fullWidth={fullWidth}
+      disabled={disabled}
+    >
       {children}
     </ButtonStyled>
   );
 };
 
 export type ButtonProps = {
-  disabled?: boolean;
   secondary?: boolean;
+  simple?: boolean;
+  fullWidth?: boolean;
+  disabled?: boolean;
   onClick?: Function;
 } & React.HTMLProps<HTMLButtonElement>;
 
 export const ButtonStyled = styled.button<ButtonProps>`
-  min-width: 100px;
-  text-align: center;
-  font-size: 1.2rem;
-  color: #fff;
-  background-color: #000;
-  border-radius: 4px;
-  border: none;
-  padding: 5px 10px;
-  margin: 10px 20px;
+  display: inline-block;
+  margin: 24px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0;
+  border-radius: 8px;
+  color: white;
+  background-color: #1e4c40;
+  font-family: "Inter", sans-serif;
+  font-weight: 600;
+  height: 56px;
+  padding: 16px 24px;
+  font-size: 1em;
+  line-height: 24px;
+  transition: all 0.2s;
   cursor: pointer;
-  transition: opacity 250ms;
-
   &:hover {
-    opacity: ${({ disabled }) => (disabled ? 1 : 0.6)};
+    background-color: #459175;
+  }
+  &:disabled {
+    background-color: lightgray;
+    cursor: not-allowed;
   }
 
   ${({ secondary }) =>
     secondary &&
     css`
       background-color: transparent;
-      outline: 1px solid #000;
-      color: #000;
+      border: 2px solid #1e4c40;
+      color: #1e4c40;
+      &:hover {
+        background-color: transparent;
+        border: 2px solid #459175;
+        color: #459175;
+      }
+      &:disabled {
+        color: lightgray;
+        background-color: transparent;
+        border: 2px solid lightgray;
+      }
     `}
 
-  ${({ disabled }) =>
-    disabled &&
+  ${({ simple }) =>
+    simple &&
     css`
-      opacity: 0.75;
-      cursor: not-allowed;
+      border: 0;
+      background-color: transparent;
+      color: #52a785;
+      &:hover {
+        background-color: transparent;
+        text-decoration: underline;
+      }
+      &:disabled {
+        background-color: transparent;
+        color: lightgrey;
+        text-decoration: none;
+      }
+    `}
+
+  ${({ fullWidth }) =>
+    fullWidth &&
+    css`
+      width: 100%;
     `}
 `;
