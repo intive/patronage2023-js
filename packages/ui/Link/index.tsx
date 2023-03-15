0bd49448daf8ce61ff432a0256c89b18d57c8f27
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-03-15 01:53:14 +0100

    feat: add Link component #P23-001

---
+++ packages/ui/Link/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import styled, { css } from "styled-components";
+import Link from "next/link";
+
+
+const Link = () => {
+    return (
+        <Link href="."></Link>
+    );
+};
