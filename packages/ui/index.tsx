41aef11eec4205a82ca9e5955f20aa9d32d14191
Bartosz Deluga <79754698+bdeluga@users.noreply.github.com>
2023-03-16 19:39:18 +0100

    feat: buttongroup-component #p23-121 (#9)
    * feat: initial commit #P23-121
    
    * feat: added icons from material-symbols
    
    * feat: added Story for ButtonGroup
    
    * feat: added secondary theme for ButtonGroup
    
    * style: move ButtonGroup stories out of Example
    
    ---------
    
    Co-authored-by: Wojciech Lechowicz <wojciech.lechowicz@intive.com>

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 export { Button } from "./Button";
+export { ButtonGroup } from './ButtonGroup'
 export { Separator } from "./Separator";
 export { Icon } from "./Icon"
 export { Chip } from "./Chip";
