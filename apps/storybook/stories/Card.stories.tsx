769f3bb0ef700dc475ebe483b0ddd995beb58dd2
Anna Kozik <anna.kozik@yahoo.pl>
2023-03-16 16:17:35 +0100

    refactor: add fit to parent #P23-114

--- apps/storybook/stories/Card.stories.tsx
+++ apps/storybook/stories/Card.stories.tsx
@@ -12,9 +12,28 @@ const Template: ComponentStory<typeof Card> = ({ children, ...args }) => (
 	<Card {...args}>{children}</Card>
 );
 
+const TemplateWithParent: ComponentStory<typeof Card> = ({
+	children,
+	...args
+}) => (
+	<div
+		style={{ width: "500px", height: "300px", backgroundColor: "lightblue" }}>
+		<Card {...args}>{children}</Card>
+	</div>
+);
+
 export const BasicCard = Template.bind({});
 BasicCard.args = {
 	padding: "20px",
 	margin: "",
 	children: <img src="form_img.png" alt="" height={350} />,
+};
+
+export const CardInParentElement = TemplateWithParent.bind({});
+CardInParentElement.args = {
+	horizontalFit: false,
+	verticalFit: false,
+	padding: "",
+	margin: "",
+	children: <img src="form_img.png" alt="" height={200} />,
 };
--- packages/ui/Card/index.tsx
+++ packages/ui/Card/index.tsx
@@ -1,28 +1,64 @@
 "use client";
 
 import { ReactNode } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export type CardProps = {
 	children: ReactNode;
 	padding?: string;
 	margin?: string;
+	horizontalFit?: boolean;
+	verticalFit?: boolean;
 };
 
-export const Card = ({ children, padding, margin }: CardProps) => {
+export const Card = ({
+	children,
+	padding,
+	margin,
+	horizontalFit,
+	verticalFit,
+}: CardProps) => {
 	return (
-		<CardStyled padding={padding} margin={margin}>
-			{children}
-		</CardStyled>
+		<CardWrapperStyled horizontalFit={horizontalFit} verticalFit={verticalFit}>
+			<CardStyled
+				padding={padding}
+				margin={margin}
+				horizontalFit={horizontalFit}
+				verticalFit={verticalFit}>
+				<CardContenetStyled>{children}</CardContenetStyled>
+			</CardStyled>
+		</CardWrapperStyled>
 	);
 };
 
+export const CardWrapperStyled = styled.div<{
+	horizontalFit?: boolean;
+	verticalFit?: boolean;
+}>`
+	display: flex;
+	max-width: 100%;
+	max-height: 100%;
+	height: ${({ verticalFit }) => verticalFit && "100%"};
+	width: ${({ horizontalFit }) => horizontalFit && "100%"};
+`;
+
 export const CardStyled = styled.div<CardProps>`
-	display: inline-block;
-	padding: ${({ padding }) => (padding ? padding : 0)};
+	width: ${({ horizontalFit }) => (horizontalFit ? "100%" : "fit-content")};
+	height: ${({ verticalFit }) => (verticalFit ? "auto" : "fit-content")};
+	padding: ${({ padding }) => (padding ? padding : "auto")};
 	margin: ${({ margin }) => (margin ? margin : 0)};
 	background-color: #ffffff;
 	border: 1px solid #e1e1e1;
 	box-shadow: 0px 2px 6px rgba(32, 41, 50, 0.1);
 	border-radius: 16px;
+	overflow: hidden;
+`;
+
+export const CardContenetStyled = styled.div`
+	overflow: auto;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
