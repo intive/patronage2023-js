5ceccdcae31190a847d951e9b490374be2e88359
Bartosz Kędziora <94013800+Alessiass@users.noreply.github.com>
2023-03-16 19:06:14 +0100

    feat: added icon component #P23-119 (#6)
    * feat: initial setup for icon component #P23-119
    
    * feat: material-symbol package added, button functionality confirmed #P23-119
    
    * feat: all buttons from design are properly added, storybook fully functional #P23-119
    
    * feat: icon variant was changed to boolean #P23-119
    
    * feat: optional iconSize prop was added #P23-119
    
    ---------
    
    Co-authored-by: Bartosz Kędziora <bartosz.kedziora1@gmail.com>
    Co-authored-by: Wojciech Lechowicz <wojciech.lechowicz@intive.com>

---
+++ apps/storybook/stories/Icon.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Icon } from "ui";
+
+export default {
+  title: "Icon",
+  component: Icon,
+} as ComponentMeta<typeof Icon>;
+
+const Template: ComponentStory<typeof Icon> = ({ ...args }) => (
+  <Icon {...args} />
+);
+
+export const Outline = Template.bind({});
+Outline.args = {
+  icon: "home",
+  filled: false,
+  color: "black",
+};
+
+export const Filled = Template.bind({});
+Filled.args = {
+  icon: "home",
+  filled: true,
+  color: "black",
+};
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
 export { Button } from "./Button";
+export { Icon } from "./Icon"
 export { Chip } from "./Chip";
 export { Logo } from "./Logo";
