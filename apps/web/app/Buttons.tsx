8aab80c9479618c3301f7eafae0172bfb646353b
zuzannapaw <zuzannapawlowska0@gmail.com>
2023-03-20 19:37:31 +0100

    fix: removed material-icons form dependencies and imports, undo changes in Buttons.tsx file #P23-120

--- apps/web/app/Buttons.tsx
+++ apps/web/app/Buttons.tsx
@@ -1,42 +1,16 @@
-"use client";
-import "material-symbols";
-import 'material-icons/iconfont/material-icons.css';
-import styled from "styled-components";
-import {NavItem,NavList } from "ui";
+import type { Metadata } from "next";
+import Buttons from "./Buttons";
 
-//creating dummy data for proper display on web
-type ObjectType = {
-  title: string;
-  active: boolean;
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Welcome to my page",
 };
 
-type DataType = Array<ObjectType>;
-const settingsData:DataType= [{title:"Edit profileeeeeeeeeeeeeeeeeeeeeeeeeeeeee",active:true},{title:"Change password",active:false},{title:"Language",active:false}];
-
-
-const SpanStyled = styled.span`
-overflow: hidden;
-text-overflow: ellipsis;
-white-space: nowrap;
-width:100%;
-`
-
-export default function Buttons() {
+export default function Web() {
   return (
-    <>
-      <NavItem active={false}><SpanStyled>Edit profile</SpanStyled></NavItem>
-      <NavItem active={true}><SpanStyled>Change password</SpanStyled></NavItem>
-
-      <NavList>
-      {settingsData.map((settingData,i) => {
-            return(
-                <NavItem active={true} key={i++}>
-                 <SpanStyled>{settingData.title}</SpanStyled>
-                </NavItem>
-                )
-            }
-        )}
-      </NavList>
-    </>
+    <div>
+      <h1>InBudget app</h1>
+      <Buttons />
+    </div>
   );
 }
