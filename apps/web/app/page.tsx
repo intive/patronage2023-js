4fc33997a0647ef8b4f1176a776725d4737e1b08
Anna Kozik <anna.kozik@yahoo.pl>
2023-03-22 18:42:54 +0100

    refactor: add ErrorDemo to web #P23-149

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,6 @@
-"use client";
 import type { Metadata } from "next";
 import Buttons from "./Buttons";
-import { Button, ErrorMessage } from "ui";
-import { useState } from "react";
+import ErrorDemo from "./ErrorDemo";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -10,23 +8,11 @@ export const metadata: Metadata = {
 };
 
 export default function Web() {
-  const [message, setMessage] = useState("");
-
-  const handleClose = () => {
-    setMessage("");
-  };
-
-  const handleClick = () => {
-    setMessage(
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque perspiciatis totam velit in veritatis pariatur accusamus earum quod labore odit eos, excepturi facere obcaecati quae libero commodi sit fugiat undNam earum nemo unde magni consequuntur nihil nesciunt atque suscipit maxime distinctio, reprehenderit maiores vitae est doloribus fugit nobis itaque iure exercitationem, impedit excepturi ducimus culpa, omnis sunt. Nemo, sequi. "
-    );
-  };
   return (
     <div>
       <h1>InBudget app</h1>
       <Buttons />
-      <Button onClick={handleClick}>Show error</Button>
-      {message && <ErrorMessage message={message} onClose={handleClose} />}
+      <ErrorDemo />
     </div>
   );
 }
