6904ca57f27867dd57debbcee77433a7fba1d2b8
Anna Kozik <95181360+aniasgit@users.noreply.github.com>
2023-03-23 14:30:57 +0100

    feat: error message component #P23-149 (#15)
    * feat: create error message component #P23-149
    
    * refactor: add changes to error message component #P23-149
    
    * refactor: resolve merging conflicts #P23-149
    
    * refactor: add ErrorDemo to web #P23-149
    
    * refactor: rename internal component #P23-149
    
    * refactor: add font #P23-149

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -5,4 +5,5 @@ export { Separator } from "./Separator";
 export { Icon } from "./Icon";
 export { Chip } from "./Chip";
 export { Logo } from "./Logo";
+export { ErrorMessage } from "./ErrorMessage";
 export { Background } from "./Background";
