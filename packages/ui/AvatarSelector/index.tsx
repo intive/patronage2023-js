104a0843299c376f9ce19c3b20bea0fd6d9426fa
Xewol <bartekdeluga0702@gmail.com>
2023-03-29 21:02:13 +0200

    refactor: changed Image to Avatar and grid to flex

--- packages/ui/AvatarSelector/index.tsx
+++ packages/ui/AvatarSelector/index.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Image from "next/image";
 import { useState } from "react";
+import { Avatar } from "../Avatar";
 export const AvatarSelector = () => {
   const [isDragging, setIsDragging] = useState(false);
 
@@ -28,28 +29,28 @@ export const AvatarSelector = () => {
         isDragging={isDragging}
       >
         <button onClick={() => console.log(`Avatar 1 selected`)}>
-          <Image src="/avatars/1.svg" alt="avatar" fill sizes="100%" />
+          <Avatar src="/avatars/1.svg" />
         </button>
         <button onClick={() => console.log(`Avatar 2 selected`)}>
-          <Image src="/avatars/2.svg" alt="avatar" fill sizes="100%" />
+          <Avatar src="/avatars/2.svg" />
         </button>
         <button onClick={() => console.log(`Avatar 3 selected`)}>
-          <Image src="/avatars/3.svg" alt="avatar" fill sizes="100%" />
+          <Avatar src="/avatars/3.svg" />
         </button>
         <button onClick={() => console.log(`Avatar 4 selected`)}>
-          <Image src="/avatars/4.svg" alt="avatar" fill sizes="100%" />
+          <Avatar src="/avatars/4.svg" />
         </button>
         <button onClick={() => console.log(`Avatar 5 selected`)}>
-          <Image src="/avatars/5.svg" alt="avatar" fill sizes="100%" />
+          <Avatar src="/avatars/5.svg" />
         </button>
         <button onClick={() => console.log(`Avatar 6 selected`)}>
-          <Image src="/avatars/6.svg" alt="avatar" fill sizes="100%" />
+          <Avatar src="/avatars/6.svg" />
         </button>
         <button onClick={() => console.log(`Avatar 7 selected`)}>
-          <Image src="/avatars/7.svg" alt="avatar" fill sizes="100%" />
+          <Avatar src="/avatars/7.svg" />
         </button>
         <button onClick={() => console.log(`Avatar 8 selected`)}>
-          <Image src="/avatars/8.svg" alt="avatar" fill sizes="100%" />
+          <Image src="/avatars/8.svg" alt="" fill sizes="100%" />
         </button>
       </Grid>
     </div>
@@ -62,29 +63,27 @@ interface GridProps {
   onTouchEnd: () => void;
 }
 const Grid = styled.div<GridProps>`
-  @media (max-width: 768px) {
-    align-items: center;
-    grid-template-rows: repeat(1, 1fr);
-    grid-template-columns: repeat(8, 1fr);
-    grid-auto-flow: row;
-    grid-auto-flow: column;
-  }
-  width: 400px;
   overflow-x: auto;
-  display: grid;
+  display: flex;
+  flex-wrap: wrap;
   padding: 1rem;
   gap: 1rem;
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: repeat(4, 1fr);
+  width: 460px;
 
   button {
     user-select: none;
     position: relative;
+    flex-shrink: 0;
     cursor: pointer;
     border-radius: 50%;
     border: none;
     height: 88px;
     width: 88px;
+    //override default Avatar styles
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
   button:hover {
     outline: 2px solid black;
@@ -98,5 +97,9 @@ const Grid = styled.div<GridProps>`
   & {
     -ms-overflow-style: none; /* IE and Edge */
     scrollbar-width: none; /* Firefox */
+  }
+
+  @media (max-width: 768px) {
+    flex-wrap: nowrap;
   }
 `;
