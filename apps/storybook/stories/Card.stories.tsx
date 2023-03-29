9feba32a5dd1278f98c05e518a8fc1c89025d8ba
Anna Kozik <anna.kozik@yahoo.pl>
2023-03-29 08:40:55 +0200

    fix: change Card to give possibility to restyled it P23-114

--- apps/storybook/stories/Card.stories.tsx
+++ apps/storybook/stories/Card.stories.tsx
@@ -14,29 +14,12 @@ const Template: ComponentStory<typeof Card> = ({ children, ...args }) => (
 
 export const BasicCard = Template.bind({});
 BasicCard.args = {
-  padding: "20px",
-  margin: "",
-  minWidth: "",
-  minHeight: "",
-  noBorder: false,
   children: (
     <img
       src="form_img.png"
       alt=""
       height={250}
-      style={{ border: "1px solid red" }}
+      style={{ border: "1px solid red", margin: "20px" }}
     />
   ),
-};
-
-export const CardWithColor = Template.bind({});
-CardWithColor.args = {
-  children:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus est accusantium eligendi? Totam asperiores nemo modi accusantium libero autem, nihil quisquam, velit numquam minus tempore quidem in dolorem soluta exercitationem.",
-  color: "#515151",
-  padding: "20px",
-  margin: "",
-  minWidth: "",
-  minHeight: "",
-  noBorder: false,
 };
