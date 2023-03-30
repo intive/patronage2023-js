42c893dc2ff65e214b891731ffe1578e1764385e
PawelPazderski <pazderski.projekt@gmail.com>
2023-03-30 16:51:34 +0200

    fix: change min resolution for desktopLowHeight #P23-148

--- apps/web/lib/css-variables.tsx
+++ apps/web/lib/css-variables.tsx
@@ -1,5 +1,5 @@
 export const device = {
   mobile: `(max-width: 767px)`,
   desktop: `(min-width: 768px)`,
-  desktopLowHeight: `(min-resolution: 150dpi)`,
+  desktopLowHeight: `(min-resolution: 144dpi)`,
 };
