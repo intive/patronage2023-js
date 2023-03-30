d4ebcbdf514f811ddbc858d183aefd4c21a803bc
Marcin Piwoński <marcin.piwonski98@gmail.com>
2023-03-30 13:45:57 +0200

    delete: story elements

--- apps/storybook/stories/SideNavigationBar.stories.tsx
+++ apps/storybook/stories/SideNavigationBar.stories.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { SideNavigationBar } from "ui";
-import { SideNavigationBarItem } from "ui";
 
 export default {
   title: "SideNavigationBar",
@@ -12,10 +11,6 @@ export default {
 const SideNavigationBarTemplate: ComponentStory<typeof SideNavigationBar> = ({
   ...args
 }) => <SideNavigationBar {...args}></SideNavigationBar>;
-
-const SideNavigationBarItemTemplate: ComponentStory<
-  typeof SideNavigationBarItem
-> = ({ ...args }) => <SideNavigationBarItem {...args}></SideNavigationBarItem>;
 
 export const SideNavBar = SideNavigationBarTemplate.bind({});
 SideNavBar.args = {
@@ -30,12 +25,4 @@ SideNavBar.args = {
     },
   ],
   pathname: "/budgets",
-};
-
-export const SideNavBarItem = SideNavigationBarItemTemplate.bind({});
-SideNavBarItem.args = {
-  href: "/budgets",
-  icon: "wallet",
-  textValue: "Budgets",
-  activeFlag: true,
 };
