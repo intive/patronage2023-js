4bc027d13a085fbbba4c7e85548367ea44474e45
Xewol <bartekdeluga0702@gmail.com>
2023-03-31 10:22:12 +0200

    refactor: added showcase of component

--- apps/web/app/AvatarSelectorDemo.tsx
+++ apps/web/app/AvatarSelectorDemo.tsx
@@ -2,5 +2,55 @@
 import React from "react";
 import { AvatarSelector } from "ui";
 export default function AvatarSelectorDemo() {
-  return <AvatarSelector />;
+  const [selectedAvatar, setSelectedAvatar] = React.useState<string>();
+
+  return (
+    <div
+      style={{
+        width: "440px",
+      }}
+    >
+      <h3>Wybrany awatar: {selectedAvatar || "brak"}</h3>
+      <AvatarSelector
+        selectedAvatar={selectedAvatar}
+        onSelect={(id) => {
+          setSelectedAvatar(id);
+        }}
+        avatars={[
+          {
+            src: "/avatars/1.svg",
+            id: "1",
+          },
+          {
+            src: "/avatars/2.svg",
+            id: "2",
+          },
+          {
+            src: "/avatars/3.svg",
+            id: "3",
+          },
+          {
+            src: "/avatars/4.svg",
+            id: "4",
+          },
+          {
+            src: "/avatars/5.svg",
+            id: "5",
+          },
+          {
+            src: "/avatars/6.svg",
+            id: "6",
+          },
+          {
+            src: "/avatars/7.svg",
+            id: "7",
+          },
+          {
+            src: "/avatars/8.svg",
+            id: "8",
+          },
+        ]}
+      />
+    </div>
+  );
 }
