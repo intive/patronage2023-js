a5fe926abbb66bb70bdaa44e1e639aace5437338
Daniel Chmur <daniel.chmur@gmail.com>
2023-04-01 12:51:41 +0200

    ref: moved styled components to another file, fixed: correct fonts not displaying

--- apps/web/app/(regflow)/layout.tsx
+++ apps/web/app/(regflow)/layout.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { device } from "lib/css-variables";
-import styled from "styled-components";
-import { Background, Card, Logo } from "ui";
+import { Logo } from "ui";
 
 import { usePathname } from "next/navigation";
+import {
+  BackgroundFlex,
+  ContentStyled,
+  CustomCard,
+  FormWrapperStyled,
+  H1Styled,
+  PageContainerStyled,
+  ParagraphStyled,
+  SectionStyled,
+} from "./layout.styles";
 
 export type LayoutProps = {
   children: React.ReactNode;
@@ -63,80 +71,3 @@ export default function RegFlowLayout({ children }: LayoutProps) {
     </BackgroundFlex>
   );
 }
-
-const BackgroundFlex = styled(Background)`
-  display: flex;
-`;
-
-const PageContainerStyled = styled.div`
-  min-height: 100%;
-  display: flex;
-  width: 100%;
-  @media ${device.desktop} {
-    max-width: 1080px;
-  }
-`;
-
-const ContentStyled = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  @media ${device.desktop} {
-    flex-direction: row;
-  }
-`;
-
-const SectionStyled = styled.div`
-  color: white;
-
-  padding: 52px 16px 0px 16px;
-
-  @media ${device.desktop} {
-    display: flex;
-    width: 50%;
-    flex-direction: column;
-    justify-content: center;
-    padding-left: 6%;
-    padding-right: 3%;
-  }
-`;
-
-const FormWrapperStyled = styled.div`
-  overflow-x: auto;
-  padding: 8px;
-
-  @media ${device.desktop} {
-    width: 50%;
-  }
-`;
-
-const H1Styled = styled.h1`
-  font-family: "Signika", sans-serif;
-  font-size: 1.5em;
-  line-height: 1.5em;
-  margin-top: 16px;
-
-  @media ${device.desktop} {
-    margin-top: 32px;
-    font-size: 2.5em;
-    line-height: 1em;
-  }
-`;
-
-const ParagraphStyled = styled.p`
-  margin-top: 8px;
-  font-size: 14px;
-  line-height: 20px;
-  margin-bottom: 24px;
-
-  @media ${device.desktop} {
-    line-height: 24px;
-    margin-top: 1em;
-  }
-`;
-
-const CustomCard = styled(Card)`
-  padding: 1em;
-`;
