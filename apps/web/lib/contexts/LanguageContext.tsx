b6072286647f8d0e24a010d6f618048173524f6b
Anna Kozik <anna.kozik@yahoo.pl>
2023-04-04 12:56:28 +0200

    refactor: resolve localStorage problem #P23-163

--- apps/web/lib/contexts/LanguageContext.tsx
+++ apps/web/lib/contexts/LanguageContext.tsx
@@ -1,26 +1,38 @@
-import { createContext, Dispatch, SetStateAction, useState } from "react";
+"use client";
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 
-enum languages {
+export enum languages {
   en = "en",
   pl = "pl",
 }
 
 interface LanguageContextInterface {
-  currentLang: languages;
-  setLang: Dispatch<SetStateAction<languages>>;
+  currentLang: languages | "";
+  setLang: Dispatch<SetStateAction<languages | "">>;
 }
 
-export const LanguageContext = createContext<LanguageContextInterface>(
-  undefined!
-);
+export const LanguageContext = createContext<LanguageContextInterface>({
+  currentLang: "",
+  setLang: () => {},
+});
 
 export const LanguageProvider = ({ children }: any) => {
-  const localLang = localStorage.getItem("lang");
-  const [lang, setLang] = useState(
-    localLang && Object.values<string>(languages).includes(localLang)
-      ? (localLang as languages)
-      : languages.en
-  );
+  const [lang, setLang] = useState<languages | "">("");
+
+  useEffect(() => {
+    const localLang = localStorage.getItem("lang");
+    setLang(
+      localLang && Object.values<string>(languages).includes(localLang)
+        ? (localLang as languages)
+        : languages.en
+    );
+  }, []);
 
   return (
     <LanguageContext.Provider value={{ currentLang: lang, setLang }}>
