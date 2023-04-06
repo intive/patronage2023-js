98c33ab7d193a545320b5cefdc6c85686f70d5bc
zuzannapaw <zuzannapawlowska0@gmail.com>
2023-04-06 16:58:49 +0200

    refactor: adjusted Textarea component #P23-213

--- packages/ui/Textarea/index.tsx
+++ packages/ui/Textarea/index.tsx
@@ -1,9 +1,14 @@
 "use client";
 import styled from "styled-components";
 
-const TextAreaStyled = styled.textarea`
-  width: 80%;
-  height: auto;
+type TextareaProps = {
+  placeholder?: string;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  className?: string;
+} & React.HTMLProps<HTMLTextAreaElement>;
+
+const TextareaStyled = styled.textarea<TextareaProps>`
+  width: 100%;
   min-height: 70px;
   font-family: "Inter", sans-serif;
   font-size: 1em;
@@ -13,10 +18,21 @@ const TextAreaStyled = styled.textarea`
   resize: none;
 
   &:focus {
+    border: 0;
     outline: 2px solid ${({ theme }) => theme.textarea.focus};
   }
 `;
 
-export const Textarea = () => {
-  return <TextAreaStyled placeholder="Description" />;
+export const Textarea = ({
+  placeholder,
+  onChange,
+  className,
+}: TextareaProps) => {
+  return (
+    <TextareaStyled
+      placeholder={placeholder}
+      onChange={onChange}
+      className={className}
+    />
+  );
 };
