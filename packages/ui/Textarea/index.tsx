394f946ce19cfacce747a1446664eaa1dd8f27c0
ZuzannaPaw <zuzannapaw@wp.pl>
2023-04-11 15:11:43 +0200

    feat: added moving label behavior #P23-213

--- packages/ui/Textarea/index.tsx
+++ packages/ui/Textarea/index.tsx
@@ -2,10 +2,14 @@
 import styled from "styled-components";
 
 type TextareaProps = {
-  placeholder?: string;
+  label?: string;
   onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
   className?: string;
 } & React.HTMLProps<HTMLTextAreaElement>;
+
+const Wrapper = styled.div`
+  position: relative;
+`;
 
 const TextareaStyled = styled.textarea<TextareaProps>`
   width: 100%;
@@ -13,26 +17,53 @@ const TextareaStyled = styled.textarea<TextareaProps>`
   font-family: "Inter", sans-serif;
   font-size: 1em;
   border-radius: 10px;
-  border: 2px solid ${({ theme }) => theme.textarea.disabled};
+  border: 0;
+  outline: 2px solid ${({ theme }) => theme.textarea.disabled};
   padding: 12px;
   resize: none;
 
   &:focus {
-    border: 0;
     outline: 2px solid ${({ theme }) => theme.textarea.focus};
+  }
+
+  &:focus,
+  &:not(:placeholder-shown) {
+    + label {
+      transform: translateY(-22px);
+      background-color: ${({ theme }) => theme.input.labelBackground};
+      font-size: 12px;
+      font-weight: 600;
+      padding-left: 4px;
+      padding-right: 4px;
+    }
   }
 `;
 
-export const Textarea = ({
-  placeholder,
-  onChange,
-  className,
-}: TextareaProps) => {
+const StyledLabel = styled.label`
+  position: absolute;
+  left: 14px;
+  top: 11px;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 150%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: calc(100% - 32px);
+  transition: all 200ms linear;
+  transition-delay: 0.25s;
+  cursor: text;
+`;
+
+export const Textarea = ({ label, onChange, className }: TextareaProps) => {
   return (
-    <TextareaStyled
-      placeholder={placeholder}
-      onChange={onChange}
-      className={className}
-    />
+    <Wrapper>
+      <TextareaStyled
+        placeholder=" "
+        onChange={onChange}
+        className={className}
+      />
+      <StyledLabel>{label}</StyledLabel>
+    </Wrapper>
   );
 };
