aba2abbb501788255a2d82a45c363328009f016b
Bartosz Kędziora <bartosz.kedziora1@gmail.com>
2023-04-11 16:51:50 +0200

    fix: styled-component elements elevated outside of main component function

--- apps/web/app/(regflow)/sign-up/PasswordSubComponent.tsx
+++ apps/web/app/(regflow)/sign-up/PasswordSubComponent.tsx
@@ -11,99 +11,99 @@ type PasswordSubComponentProps = {
   onBack: () => void;
 };
 
+const StyledSubmitButton = styled(Button)`
+  width: 75%;
+`;
+
+const StyledBackButton = styled(Button)`
+  width: 23%;
+`;
+
+const StyledHeader = styled.h2`
+  color: ${({ theme }) => theme.text.header};
+  font-family: "Signika", sans-serif;
+  font-size: 1.5em;
+  text-align: center;
+`;
+
+const StyledSubHeader = styled.h3`
+  margin-top: 4px;
+  font-family: "Inter", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1em;
+  text-align: center;
+  color: ${({ theme }) => theme.text.paragraph};
+`;
+
+const ListHeader = styled.p`
+  font-family: "Inter", sans-serif;
+  margin-bottom: 5px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.text.header};
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+
+  & > * {
+    flex-basis: 85px;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ListWrapper = styled.div`
+  display: flex;
+  min-height: 150px;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 0 16px;
+`;
+
+const UnorderedListWrapper = styled.ul`
+  list-style: none;
+
+  & > li {
+    font-family: "Inter", sans-serif;
+    background: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAiIGhlaWdodD0iMiIgdmlld0JveD0iMCAwIDEwIDIiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0xLjQ1ODY2IDEuOTc5MDhDMS4xODA4OCAxLjk3OTA4IDAuOTQ4MjQyIDEuODg1MzMgMC43NjA3NDIgMS42OTc4M0MwLjU3MzI0MiAxLjUxMDMzIDAuNDc5NDkyIDEuMjc3NyAwLjQ3OTQ5MiAwLjk5OTkxOEMwLjQ3OTQ5MiAwLjcyMjE0MSAwLjU3MzI0MiAwLjQ4OTUwMiAwLjc2MDc0MiAwLjMwMjAwMkMwLjk0ODI0MiAwLjExNDUwMiAxLjE4MDg4IDAuMDIwNzUyIDEuNDU4NjYgMC4wMjA3NTJIOC41NDE5OUM4LjgxOTc3IDAuMDIwNzUyIDkuMDUyNDEgMC4xMTQ1MDIgOS4yMzk5MSAwLjMwMjAwMkM5LjQyNzQxIDAuNDg5NTAyIDkuNTIxMTYgMC43MjIxNDEgOS41MjExNiAwLjk5OTkxOEM5LjUyMTE2IDEuMjc3NyA5LjQyNzQxIDEuNTEwMzMgOS4yMzk5MSAxLjY5NzgzQzkuMDUyNDEgMS44ODUzMyA4LjgxOTc3IDEuOTc5MDggOC41NDE5OSAxLjk3OTA4SDEuNDU4NjZaIiBmaWxsPSIjNjRCQTk1Ii8+Cjwvc3ZnPgo=")
+      no-repeat left center;
+    padding: 5px 10px 5px 25px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    color: ${({ theme }) => theme.text.paragraph};
+  }
+`;
+
+const FormWrapper = styled.div`
+  display: flex;
+  height: 542px;
+  width: 416px;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  padding: 30px 0;
+
+  @media ${device.mobile} {
+    width: 311px;
+  }
+`;
+
 export const PasswordSubComponent = ({
   onNext,
   onBack,
 }: PasswordSubComponentProps) => {
   const { t, dict } = useTranslate("SignUpPage");
   const { passwordComponent } = dict;
-
-  const StyledSubmitButton = styled(Button)`
-    width: 75%;
-  `;
-
-  const StyledBackButton = styled(Button)`
-    width: 23%;
-  `;
-
-  const StyledHeader = styled.h2`
-    color: ${({ theme }) => theme.text.header};
-    font-family: "Signika", sans-serif;
-    font-size: 1.5em;
-    text-align: center;
-  `;
-
-  const StyledSubHeader = styled.h3`
-    margin-top: 4px;
-    font-family: "Inter", sans-serif;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 1em;
-    text-align: center;
-    color: ${({ theme }) => theme.text.paragraph};
-  `;
-
-  const ListHeader = styled.p`
-    font-family: "Inter", sans-serif;
-    margin-bottom: 5px;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 20px;
-    color: ${({ theme }) => theme.text.header};
-  `;
-
-  const InputWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-
-    & > * {
-      flex-basis: 85px;
-    }
-  `;
-
-  const ButtonWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-  `;
-
-  const ListWrapper = styled.div`
-    display: flex;
-    min-height: 150px;
-    flex-direction: column;
-    justify-content: space-around;
-    padding: 0 16px;
-  `;
-
-  const UnorderedListWrapper = styled.ul`
-    list-style: none;
-
-    & > li {
-      font-family: "Inter", sans-serif;
-      background: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAiIGhlaWdodD0iMiIgdmlld0JveD0iMCAwIDEwIDIiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0xLjQ1ODY2IDEuOTc5MDhDMS4xODA4OCAxLjk3OTA4IDAuOTQ4MjQyIDEuODg1MzMgMC43NjA3NDIgMS42OTc4M0MwLjU3MzI0MiAxLjUxMDMzIDAuNDc5NDkyIDEuMjc3NyAwLjQ3OTQ5MiAwLjk5OTkxOEMwLjQ3OTQ5MiAwLjcyMjE0MSAwLjU3MzI0MiAwLjQ4OTUwMiAwLjc2MDc0MiAwLjMwMjAwMkMwLjk0ODI0MiAwLjExNDUwMiAxLjE4MDg4IDAuMDIwNzUyIDEuNDU4NjYgMC4wMjA3NTJIOC41NDE5OUM4LjgxOTc3IDAuMDIwNzUyIDkuMDUyNDEgMC4xMTQ1MDIgOS4yMzk5MSAwLjMwMjAwMkM5LjQyNzQxIDAuNDg5NTAyIDkuNTIxMTYgMC43MjIxNDEgOS41MjExNiAwLjk5OTkxOEM5LjUyMTE2IDEuMjc3NyA5LjQyNzQxIDEuNTEwMzMgOS4yMzk5MSAxLjY5NzgzQzkuMDUyNDEgMS44ODUzMyA4LjgxOTc3IDEuOTc5MDggOC41NDE5OSAxLjk3OTA4SDEuNDU4NjZaIiBmaWxsPSIjNjRCQTk1Ii8+Cjwvc3ZnPgo=")
-        no-repeat left center;
-      padding: 5px 10px 5px 25px;
-      font-style: normal;
-      font-weight: 400;
-      font-size: 14px;
-      color: ${({ theme }) => theme.text.paragraph};
-    }
-  `;
-
-  const FormWrapper = styled.div`
-    display: flex;
-    height: 542px;
-    width: 416px;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    justify-content: space-between;
-    padding: 30px 0;
-
-    @media ${device.mobile} {
-      width: 311px;
-    }
-  `;
 
   return (
     <>
