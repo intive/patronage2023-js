099c530d7bd5ed31212ee4dd8ab9941dcb5a16f7
Xewol <bartekdeluga0702@gmail.com>
2023-04-13 00:17:08 +0200

    feat: added flow controller - note to self: ts sucks

--- apps/web/app/(regflow)/sign-up/SignUp.tsx
+++ apps/web/app/(regflow)/sign-up/SignUp.tsx
@@ -1,6 +1,122 @@
 "use client";
 import { FlowController } from "ui";
+import { EmailScreen } from "./EmailScreen";
+import { PasswordSubComponent } from "./PasswordSubComponent";
+import { ProfileScreen } from "./ProfileScreen";
+import { SuccessErrorScreen } from "./SuccessErrorScreen";
+import { useState } from "react";
+
+interface userObject {
+  email: string;
+  password: string;
+  profile: {
+    firstName: string;
+    lastName: string;
+    avatar: string;
+  };
+}
 
 export const SignUp = () => {
-  return <FlowController />;
+  const [currentStep, setCurrentStep] = useState(0);
+  const [result, setResult] = useState<number | null>(null); // 200, 400
+  const [user, setUser] = useState<userObject>({
+    email: "",
+    password: "",
+    profile: {
+      firstName: "",
+      lastName: "",
+      avatar: "",
+    },
+  });
+
+  const goBack = () => setCurrentStep(currentStep - 1);
+
+  const goToBeginning = () => {
+    setCurrentStep(0);
+  };
+  const validateEmail = (email: string) => {
+    // get validated email and set it to user
+    setUser({ ...user, email });
+
+    //go next
+    setCurrentStep(currentStep + 1);
+  };
+  const validatePassword = (password: string) => {
+    //get validated not hashed password and set it to user
+    setUser({ ...user, password });
+
+    //go next
+    setCurrentStep(currentStep + 1);
+  };
+
+  const validateProfile = (profileInfo: userObject["profile"]) => {
+    //set profile info to user
+    setUser({ ...user, profile: profileInfo });
+
+    //useState might not be updated here, so we use fetch with passed profileInfo directly
+    //   fetch("http://strzelam_w_backend:5000", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       email: user.email,
+    //       password: hashedPassword,
+    //       profile: profileInfo,
+    //     }),
+    //   }).then((res) => {
+    //     setResult(res.status);
+    //   });
+    // };
+
+    //alert for showcase
+    alert(
+      JSON.stringify(
+        {
+          email: user.email,
+          password: user.password,
+          ...profileInfo,
+        },
+        null,
+        2
+      )
+    );
+  };
+  console.log(user);
+
+  return (
+    <FlowController
+      counter={currentStep}
+      steps={[
+        {
+          screen: EmailScreen,
+          props: {
+            onNext: validateEmail,
+            onBack: goBack,
+            user: user.email,
+          },
+        },
+        {
+          screen: PasswordSubComponent,
+          props: {
+            onNext: validatePassword,
+            onBack: goBack,
+            user: user.password,
+          },
+        },
+        {
+          screen: ProfileScreen,
+          props: {
+            done: validateProfile,
+            back: goBack,
+            user: user.profile,
+          },
+        },
+        {
+          screen: SuccessErrorScreen,
+          props: {},
+        },
+      ]}
+    />
+  );
 };
