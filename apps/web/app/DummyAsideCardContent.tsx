6793ff2ac4942ba2c9ed2f9101af6d91aac392cc
zuzannapaw <zuzannapawlowska0@gmail.com>
2023-04-12 21:09:07 +0200

    refactor: removed list from DummyAsideCardContent.tsx #P23-199

--- apps/web/app/DummyAsideCardContent.tsx
+++ apps/web/app/DummyAsideCardContent.tsx
@@ -3,18 +3,6 @@ import styled from "styled-components";
 import { CheckboxWrapper } from "./CheckboxWrapper";
 
 //creating imitation of RightCard content
-const dummyContents = [
-  { title: "Home spendings", iconName: "home", color: "#1E4C40", id: 1 },
-  {
-    title: "Subscriptions",
-    iconName: "subscriptions",
-    color: "#643400",
-    id: 2,
-  },
-  { title: "Car", iconName: "directions_car", color: "#003150", id: 3 },
-  { title: "Grocery", iconName: "shopping_cart", color: "#5A092F", id: 4 },
-];
-
 const DescriptionsWrapper = styled.div`
   display: flex;
   width: 100%;
@@ -23,11 +11,7 @@ const DescriptionsWrapper = styled.div`
   padding: 0px 20px;
   margin-top: 15px;
 `;
-export const H3Styled = styled.h3`
-  font-size: 0.9em;
-  font-weight: 500;
-  font-family: "Inter", sans-serif;
-`;
+
 const CheckboxesWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -37,9 +21,14 @@ const CheckboxesWrapper = styled.div`
   padding: 20px;
 `;
 
-//it stands for dummy component that will be passed into MultiCardLayout in AsideCard
+export const H3Styled = styled.h3`
+  font-size: 0.9em;
+  font-weight: 500;
+  font-family: "Inter", sans-serif;
+`;
+
+//it stands for component that will be passed into MultiCardLayout in AsideCard
 //did not yet applied dictionary because it's a dummy component only for initial presentation
-//red mark because of icon types
 export const DummyAsideCardContent = () => {
   return (
     <>
@@ -48,16 +37,26 @@ export const DummyAsideCardContent = () => {
         <H3Styled>Manage</H3Styled>
       </DescriptionsWrapper>
       <CheckboxesWrapper>
-        {dummyContents.map((content) => {
-          return (
-            <CheckboxWrapper
-              title={content.title}
-              iconName={content.iconName}
-              color={content.color}
-              key={content.id}
-            />
-          );
-        })}
+        <CheckboxWrapper
+          title="Home spendings"
+          iconName="home"
+          color="#1E4C40"
+        />
+        <CheckboxWrapper
+          title="Subscriptions"
+          iconName="subscriptions"
+          color="#643400"
+        />
+        <CheckboxWrapper
+          title="Car"
+          iconName="directions_car"
+          color="#003150"
+        />
+        <CheckboxWrapper
+          title="Grocery"
+          iconName="shopping_cart"
+          color="#5A092F"
+        />
       </CheckboxesWrapper>
     </>
   );
