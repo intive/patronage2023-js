a4de9879344a9a199431b27a7e03054716da9222
Daniel Chmur <daniel.chmur@gmail.com>
2023-04-12 12:01:11 +0200

    feat: atomic component - datepicker #P23-214 (#37)
    * git
    
    * chore: styling datepicker
    
    * style: adjusting styles only
    
    * chore: started making UI component
    
    * style: styled year piker
    
    * chore: added onSelect function
    
    * fix: fixed TS typing, fixed calendar clipping with parent object -> now it can render itself over parent element
    
    * style: improved styling of year picker in calendar
    
    * a
    
    * Revert "a"
    
    This reverts commit 141a2bb81d14ef126ab318ac38d41adac67d7ad6.
    
    * Revert "Revert "a""
    
    This reverts commit b199e9fa999de217b2d925c19d33bf7121b85d09.
    
    * fix: just fixing commit
    
    * ref: changed test component folder and added another example of usage
    
    * chore: added experimental locale changing feature
    
    * ref: added default language
    
    * style: adjusted visuals on smallest screens
    
    * ref: reverted language settings back to default
    
    * chore: storybook story added
    
    * ref: made en-US as a default language
    
    * style: datePicker theme added
    
    * ref: moved localization logic to component itself
    
    * fix: disabled native keyboard on mobile phones when date input is focused
    
    * fix: simplified component locaization -> it's now using datePickerLanguageConfig ojectt instead of useEffect and switch
    
    * fix: fixed build problems
    
    * style: made year scroll lighter and smaller
    
    * fix: added example of four types of datepickers
    
    * ref: reverted component code state back to last version
    
    * ref: removed redundant type
    
    * fix: build problems
    
    * ref: removed redundand prop from datepicker
    
    * Revert "fix: added example of four types of datepickers"
    
    This reverts commit db8df12317025ca96574e44ce2e7d699f60f9174.
    
    * fix: pulled main

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -14,6 +14,7 @@ export { LinkComponent } from "./Link";
 export { AvatarSelector } from "./AvatarSelector";
 export { Input } from "./Input";
 export { Avatar } from "./Avatar";
+export { CustomDatePicker } from "./DatePicker";
 export { Checkbox } from "./Checkbox";
 export { Textarea } from "./Textarea";
 export { IconPicker } from "./IconPicker";
