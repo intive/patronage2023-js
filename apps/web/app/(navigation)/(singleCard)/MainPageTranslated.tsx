7e013b1790a1e78f9008b9548c02b0a940cf9236
ZuzannaPaw <zuzannapaw@wp.pl>
2023-04-12 14:32:36 +0200

    refactor: started implementing new behavior of layout #P23-199

--- apps/web/app/(navigation)/(singleCard)/MainPageTranslated.tsx
+++ apps/web/app/(navigation)/(singleCard)/MainPageTranslated.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { CardStyled, TypoStyled, LinkStyled } from "app/(navigation)/HomePageComponents";
+import {
+  CardStyled,
+  TypoStyled,
+  LinkStyled,
+} from "app/(navigation)/HomePageComponents";
 import { useTranslate } from "lib/hooks";
 
 export const MainPageTranslated = () => {
   const { dict, t } = useTranslate("MainPage");
 
   return (
-    <CardStyled>
+    <>
       <TypoStyled>{t(dict.welcomeText)}</TypoStyled>
       <LinkStyled href="/sign-in">{t(dict.createAccountLink)}</LinkStyled>
-    </CardStyled>
+    </>
   );
 };
