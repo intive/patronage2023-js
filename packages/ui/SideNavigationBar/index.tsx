274e2e7040cc7f9731a6a074a537bd9bf4b3a4c1
zuzannapaw <zuzannapawlowska0@gmail.com>
2023-04-13 14:10:48 +0200

    styles: removed % for margin #P23-199

--- packages/ui/SideNavigationBar/index.tsx
+++ packages/ui/SideNavigationBar/index.tsx
@@ -33,7 +33,7 @@ const SideNavigationBarStyled = styled.ul<SideNavigationBarProps>`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  margin-top: 0.5%;
+  padding-top: 40px;
   padding: 0;
   list-style: none;
 `;
