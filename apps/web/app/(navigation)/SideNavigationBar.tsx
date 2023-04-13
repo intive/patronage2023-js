4bb25fd14aadb0292f7059f28628c94747be7300
Marcin Piwoński <43062706+mpiwonski@users.noreply.github.com>
2023-04-13 16:22:25 +0200

    feat: side-navigation-bar-submenu #P23-197 (#42)
    * feat: add sidebar submenu (in progress) #P23-197
    
    * fix: undefined (no return) #P23-197
    
    * fix: unsuccessful build, add searchInput #P23-197
    
    * refactor: toggling state (link) + styles #P23-197
    
    * refactor: active state, types #P23-197
    
    * refactor: active state; DRY rule #P23-197
    
    * refactor: add input base (in progress) #P23-197
    
    * refactor: add NavList; delete IconProps #P23-197
    
    * refactor: code review changes #P23-197
    
    * refactor: add theme colors #P23-197
    
    * refactor: code review changes #P23-197
    
    * refactor: code review changes - cont #P23-197

--- apps/web/app/(navigation)/SideNavigationBar.tsx
+++ apps/web/app/(navigation)/SideNavigationBar.tsx
@@ -1,39 +1,105 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import { SideNavigationBar } from "ui";
 import { useTranslate } from "lib/hooks";
+import { usePathname } from "next/navigation";
+import { SideNavigationBar, Icon, NavList } from "ui";
+
+import {
+  BudgetsSubMenuNavListContents,
+  TeamSubMenuNavListContents,
+  SettingsSubMenuNavListContents,
+} from "./SideNavigationBarNavListData";
 
 export default function SideNav() {
-  const pathname = usePathname() || "";
   const { dict, t } = useTranslate("NavigationLayout");
   const { SideNav } = dict;
+  const currentPage = usePathname() || "";
+
+  const BudgetsSubMenuData = {
+    title: "Budgets",
+    sort: {
+      method: () => {},
+      icon: <Icon icon="filter_list" />,
+    },
+    searchInput: {
+      placeholder: "Search budgets",
+      icon: <Icon icon="search" color="#515151" />,
+    },
+    navigationList: (
+      <NavList
+        contents={BudgetsSubMenuNavListContents}
+        currentPage={currentPage}
+      />
+    ),
+    button: {
+      method: () => {},
+      label: "Add new budget",
+    },
+  };
+
+  const TeamSubMenuData = {
+    title: "Team",
+    sort: {
+      method: () => {},
+      icon: <Icon icon="filter_list" />,
+    },
+    searchInput: {
+      icon: <Icon icon="search" color="#515151" />,
+      placeholder: "Search team",
+    },
+    navigationList: (
+      <NavList
+        contents={TeamSubMenuNavListContents}
+        currentPage={currentPage}
+      />
+    ),
+    button: {
+      method: () => {},
+      label: "Add new member",
+    },
+  };
+
+  const SettingsSubMenuData = {
+    title: "Settings",
+    navigationList: (
+      <NavList
+        contents={SettingsSubMenuNavListContents}
+        currentPage={currentPage}
+      />
+    ),
+  };
 
   return (
     <SideNavigationBar
       items={[
         {
           href: "/budgets",
-          icon: "wallet",
+          icon: <Icon icon="wallet" iconSize={32} />,
           textValue: t(SideNav.budgetsItem),
+          subMenu: BudgetsSubMenuData,
+          id: 1,
         },
         {
           href: "/reports",
-          icon: "query_stats",
+          icon: <Icon icon="query_stats" iconSize={32} />,
           textValue: t(SideNav.reportsItem),
+          id: 2,
         },
         {
           href: "/team",
-          icon: "account_circle",
+          icon: <Icon icon="account_circle" iconSize={32} />,
           textValue: t(SideNav.teamsItem),
+          subMenu: TeamSubMenuData,
+          id: 3,
         },
         {
           href: "/settings",
-          icon: "settings",
+          icon: <Icon icon="settings" iconSize={32} />,
           textValue: t(SideNav.settingsItem),
+          subMenu: SettingsSubMenuData,
+          id: 4,
         },
       ]}
-      pathname={pathname}
     />
   );
 }
---
+++ packages/ui/SideNavigationBar/SubMenu/index.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { ReactNode } from "react";
+import { SearchInput } from "../../Input/SearchInput";
+import { Button } from "../../Button";
+
+export type SubMenuDataProps = {
+  title: string;
+  sort?: {
+    method: () => void;
+    icon: ReactNode;
+  };
+  searchInput?: {
+    placeholder: string;
+    icon: ReactNode;
+  };
+  navigationList?: ReactNode;
+  button?: {
+    method: () => void;
+    label: string;
+  };
+};
+
+type SubMenuProps = {
+  subMenuDataObject: SubMenuDataProps;
+} & React.HTMLProps<HTMLDivElement>;
+
+const SubMenuStyled = styled.div`
+  width: 288px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  padding: 40px 16px 24px 16px;
+
+  border-left: 1px solid
+    ${({ theme }) => theme.sideNavigationBar.subMenu.separator};
+`;
+
+export const SubMenu = ({ subMenuDataObject: subMenuData }: SubMenuProps) => {
+  const { title, sort, searchInput, navigationList, button } = subMenuData;
+  return (
+    <SubMenuStyled>
+      <MainDiv>
+        <HeaderStyled>
+          <Title>{title}</Title>
+          {sort?.icon}
+        </HeaderStyled>
+        {searchInput && <SearchInput searchInput={searchInput} />}
+        {navigationList}
+      </MainDiv>
+
+      {button && (
+        <Button
+          variant="secondary"
+          onClick={() => console.log("Modal has been opened! ")}>
+          {button.label}
+        </Button>
+      )}
+    </SubMenuStyled>
+  );
+};
+
+const HeaderStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Title = styled.span`
+  font-family: "Signika";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  color: ${({ theme }) => theme.sideNavigationBar.subMenu.title};
+  line-height: 36px;
+`;
+
+const MainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
