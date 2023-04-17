710f6fae6505a87669af1f6176fe5b8ef63c39f0
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-04-17 23:44:22 +0200

    style: green TAG on focus in option list #P23-212

--- packages/ui/CurrencySelect/index.tsx
+++ packages/ui/CurrencySelect/index.tsx
@@ -1,35 +1,33 @@
 "use client";
 
-import * as React from "react";
 import * as Select from "@radix-ui/react-select";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { Icon } from "../Icon";
 
-{/* <Icon icon="arrow_drop_down" iconSize={24} /> */}
-
 export type CurrencySelectComponentProps = {
-  tag: string;
-  label: string;
-  id: string | number;
-  value: string;
+  // tag: string;
+  // label: string;
+  // id: string | number;
+  // value: string;
 } & React.HTMLProps<HTMLElement>;
 
-export const CurrencySelect = ({ 
-}: CurrencySelectComponentProps) => {
+export const CurrencySelect = ({}: CurrencySelectComponentProps) => {
   return (
     <Select.Root>
       <SelectTrigger>
-      {/* <StyledLabel>Currency</StyledLabel> */}
-        <SelectValue placeholder="Currency"/>
-        <SelectIcon><Icon icon="arrow_drop_down" iconSize={23} /></SelectIcon>
+        <Select.Value placeholder="Currency"></Select.Value>
+        <SelectIcon>
+          <Icon icon="arrow_drop_down" iconSize={23} />
+        </SelectIcon>
       </SelectTrigger>
-      
       <SelectPortal>
-        <SelectContent position="popper" >
-          <SelectViewport className="SelectViewport">
+        <SelectContent position="popper">
+          <SelectViewport>
             {currency.map((currency) => (
               <SelectItem value={currency.id}>
-                <SelectItemText><StyledTag>{currency.tag}</StyledTag> {currency.label}</SelectItemText>
+                <SelectItemText>
+                  <StyledTag>{currency.tag}</StyledTag> {currency.label}
+                </SelectItemText>
               </SelectItem>
             ))}
           </SelectViewport>
@@ -40,47 +38,28 @@ export const CurrencySelect = ({
 };
 
 const SelectTrigger = styled(Select.Trigger)`
-  background-color: ${({theme}) => theme.card.background};
+  background-color: ${({ theme }) => theme.card.background};
   border: solid 2px ${({ theme }) => theme.input.borderError};
   border-radius: 8px;
   font-size: 1em;
-  min-width: 13em;
+  width: 100%;
   height: 56px;
   cursor: pointer;
   display: flex;
   justify-content: space-between;
   padding-top: 1em;
-  padding-left: 16px;
+  padding-left: 1em;
   padding-right: 10px;
 
-  :focus {
+  &:focus {
     outline: none;
     border-color: ${({ theme }) => theme.input.focus};
   }
 `;
 
-const SelectValue = styled(Select.Value)`
-
-`;
-
-
-// const StyledLabel = styled.label`
-//   position: absolute;
-//   display: flex;
-//   font-size: 12px;
-//   font-weight: 600;
-//   background-color: ${({ theme }) => theme.input.labelBackground};
-//   color: ${({ theme }) => theme.input.neutral};
-//   padding-left: 4px;
-//   padding-right: 4px;
-//   margin-left: 12px;
-//   top: -7px;
-// `;
-
 const SelectIcon = styled(Select.Icon)`
   color: #626262;
   margin-top: -2px;
-  /* margin-right: 15px; */
 `;
 
 const SelectPortal = styled(Select.Portal)`
@@ -93,11 +72,13 @@ const SelectContent = styled(Select.Content)`
   background-color: white;
   border: solid 1px ${({ theme }) => theme.input.borderError};
   cursor: pointer;
+  
 `;
 
+
+
 const SelectViewport = styled(Select.Viewport)`
-  box-shadow: 0px 2px 8px rgba(32, 37, 50, 0.08), 0px 2px 4px rgba(32, 37, 50, 0.03);
-  min-width: 13em;
+  box-shadow: 0px 2px 4px 0px #20253208;
 `;
 
 const SelectItem = styled(Select.Item)`
@@ -105,54 +86,48 @@ const SelectItem = styled(Select.Item)`
   outline-color: ${({ theme }) => theme.input.focus};
   padding: 16px;
   gap: 8px;
-  
+
   &:focus {
-    color: #397B65;
-    background-color: #F1FBF6;
-    :first-child {
+    color: #397b65;
+    background-color: #f1fbf6;
+    &:first-child {
       border-radius: 1em 1em 0 0;
     }
-    :last-child {
+    &:last-child {
       border-radius: 0 0 1em 1em;
     }
   }
-
-  /* &:hover {
-    color: #397B65;
-  } */
 `;
 
-const StyledTag = styled.span`
-  color: #515151;
-`;
+
 
 const SelectItemText = styled(Select.ItemText)`
   color: #222222;
+`;
+
+const StyledTag = styled.span`
+  color: #515151; 
 `;
 
 const currency = [
   {
     tag: "PLN",
     label: "Polish Zloty",
-    name: "PLN Polish Zloty",
     id: "1",
   },
   {
     tag: "GBP",
     label: "British Pound",
-    name: "GBP British Pound",
     id: "2",
   },
   {
     tag: "EUR",
     label: "Euro",
-    name: "EUR Euro",
     id: "3",
   },
   {
     tag: "USD",
     label: "United States Dollar",
-    name: "USD United States Dollar",
     id: "4",
   },
 ];
