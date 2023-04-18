fe1ede844c9e97e25205775cb5a11319c66fcf42
Daniel Chmur <daniel.chmur@gmail.com>
2023-04-18 09:36:10 +0200

    Revert "fix: fixed storybook of CustomDatePicker"
    This reverts commit 877114cf03e633ac7f433ee1d81890c3a5f16f37.

--- apps/storybook/stories/CustomDatePicker.stories.tsx
+++ apps/storybook/stories/CustomDatePicker.stories.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import styled from "styled-components";
 
 import { CustomDatePicker } from "ui";
-import React from "react";
 
 export default {
   title: "CustomDatePicker",
@@ -14,22 +13,25 @@ const Wrapper = styled.div`
   width: 311px;
 `;
 
-const Template: ComponentStory<typeof CustomDatePicker> = ({ ...args }) => {
-  const [value, setValue] = useState<Date | null>(null);
-
-  return (
-    <Wrapper>
-      <CustomDatePicker
-        {...args}
-        onSelect={(date) => setValue(date)}
-        selected={value}
-      />
-    </Wrapper>
-  );
-};
+const Template: ComponentStory<typeof CustomDatePicker> = ({ ...args }) => (
+  <Wrapper>
+    <CustomDatePicker {...args} />
+  </Wrapper>
+);
 
 export const Default = Template.bind({});
-
 Default.args = {
-  label: "Label",
+  placeholder: "Placeholder",
+  onSelect: (date: Date) => {
+    console.log(date);
+  },
+};
+
+export const TranslatedToPolish = Template.bind({});
+TranslatedToPolish.args = {
+  placeholder: "Zaślepka",
+  onSelect: (date: Date) => {
+    console.log(date);
+  },
+  lang: "pl",
 };
