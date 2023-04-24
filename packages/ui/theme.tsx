0af5dfff640956cc45675909f3f37a83615111ab
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-04-24 10:46:10 +0200

    Feature/p23 212 selector (#49)
    * feat: created Selector component files #P23-212
    
    * feat: add story to StoryBook #P23-212
    
    * fix: change component name in StoryBook #P23-212
    
    * refactor: add neautralLabel color toinput #P23-212
    
    * refactor: add CurrencySelect to ui #P23-212
    
    * feat: add part of Currency Select #P23-212
    
    * feat: add .json for Radix's select #P23-212
    
    * feat: added most of select component #P23-212
    
    * style: add focus style in option list #P23-212
    
    * feat: Current version of Currency Select #P23-212
    
    * fix: fix TS id error #P23-212
    
    * fix: delete CurrencySelect from home page #P23-212
    
    * style: styles based on radix constraints #P23-212
    
    * style: green TAG on focus in option list #P23-212
    
    * style: display only TAG after select #P23-212
    
    * style: add z-index to display option #P23-212
    
    * feat: add CurrencySelect to New Budget #P23-212
    
    * style: change label color #P23-212
    
    * feat: changing arrow icon on open list #P23-212
    
    * feat: add hasError and supp. error label #P23-212
    
    * feat: add onValueChange #P23-212
    
    * feat: add colors for currency select #P23-212
    
    * style: change color to theme's colors #P23-212
    
    * chore: change elements order #P23-212
    
    * fix: fix hasError type Error #P23-212
    
    * feat: add error to StoryBook #P23-212

--- packages/ui/theme.tsx
+++ packages/ui/theme.tsx
@@ -98,6 +98,7 @@ export const theme = {
     error: colors.Supporting4,
     borderError: colors.Neutral2,
     neutral: colors.Neutral8,
+    neutralLabel: colors.Neutral10,
     focus: colors.Teal5,
     labelBackground: colors.BasicWhite,
   },
@@ -190,6 +191,13 @@ export const theme = {
     link: colors.BasicWhite,
     accent: colors.Neutral2,
   },
+  currencySelect: {
+    background: colors.BasicWhite,
+    icon: colors.Neutral7,
+    focusBackground: colors.Teal1,
+    tag: colors.Neutral8,
+    tagFocus: colors.Teal8,
+  }
 };
 
 export type ThemeType = typeof theme;
