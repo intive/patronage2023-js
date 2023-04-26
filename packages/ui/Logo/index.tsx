d6177563071b0f99be52d72eb4e97fd74f7f1cc5
Daniel Chmur <daniel.chmur@gmail.com>
2023-04-26 15:47:04 +0200

    fix: simplified structure of LogoSignet styled component to prevent prettier from causing errors

--- packages/ui/Logo/index.tsx
+++ packages/ui/Logo/index.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 import styled, { css } from "styled-components";
 
 const logoVersions = {
-  colorURL:
+  color:
     "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjgiIGhlaWdodD0iMzgiIHZpZXdCb3g9IjAgMCAyOCAzOCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTAgMTEuNTgwNkMwIDEwLjI2ODQgMC44NTI3NDkgOS4xMDg1MiAyLjEwNTE4IDguNzE3MTNMMjQuMTA1MiAxLjg0MjEzQzI2LjAzNyAxLjIzODQ1IDI4IDIuNjgxNjUgMjggNC43MDU1N1YxOS40MTk0QzI4IDIwLjczMTYgMjcuMTQ3MyAyMS44OTE1IDI1Ljg5NDggMjIuMjgyOUwzLjg5NDgzIDI5LjE1NzlDMS45NjMwNCAyOS43NjE2IDAgMjguMzE4MyAwIDI2LjI5NDRWMTEuNTgwNloiIGZpbGw9IiM1M0E3ODUiLz4KPHBhdGggb3BhY2l0eT0iMC42IiBkPSJNMCAxMS43MDU2QzAgOS42ODE2NSAxLjk2MzAzIDguMjM4NDUgMy44OTQ4MiA4Ljg0MjEzTDI1Ljg5NDggMTUuNzE3MUMyNy4xNDczIDE2LjEwODUgMjggMTcuMjY4NCAyOCAxOC41ODA2VjMzLjI5NDRDMjggMzUuMzE4MyAyNi4wMzcgMzYuNzYxNiAyNC4xMDUyIDM2LjE1NzlMMi4xMDUxOCAyOS4yODI5QzAuODUyNzQ5IDI4Ljg5MTUgMCAyNy43MzE2IDAgMjYuNDE5NFYxMS43MDU2WiIgZmlsbD0iIzgyRDNBRiIvPgo8L3N2Zz4K",
-  whiteURL:
+  white:
     "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjgiIGhlaWdodD0iMzgiIHZpZXdCb3g9IjAgMCAyOCAzOCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTAgMTEuNTgwNkMwIDEwLjI2ODQgMC44NTI3NDkgOS4xMDg1MiAyLjEwNTE4IDguNzE3MTNMMjQuMTA1MiAxLjg0MjEzQzI2LjAzNyAxLjIzODQ1IDI4IDIuNjgxNjUgMjggNC43MDU1N1YxOS40MTk0QzI4IDIwLjczMTYgMjcuMTQ3MyAyMS44OTE1IDI1Ljg5NDggMjIuMjgyOUwzLjg5NDgzIDI5LjE1NzlDMS45NjMwNCAyOS43NjE2IDAgMjguMzE4MyAwIDI2LjI5NDRWMTEuNTgwNloiIGZpbGw9IiNGQ0ZDRkMiLz4KPHBhdGggb3BhY2l0eT0iMC42IiBkPSJNMCAxMS43MDU2QzAgOS42ODE2NSAxLjk2MzAzIDguMjM4NDUgMy44OTQ4MiA4Ljg0MjEzTDI1Ljg5NDggMTUuNzE3MUMyNy4xNDczIDE2LjEwODUgMjggMTcuMjY4NCAyOCAxOC41ODA2VjMzLjI5NDRDMjggMzUuMzE4MyAyNi4wMzcgMzYuNzYxNiAyNC4xMDUyIDM2LjE1NzlMMi4xMDUxOCAyOS4yODI5QzAuODUyNzQ5IDI4Ljg5MTUgMCAyNy43MzE2IDAgMjYuNDE5NFYxMS43MDU2WiIgZmlsbD0iI0QyRTFEQiIvPgo8L3N2Zz4K",
 };
 
@@ -15,14 +15,11 @@ type LogoProps = {
   logoWidth?: number;
 };
 
-type LogoSignetProps = Pick<LogoProps, "logoWidth" | "white">;
-
 type ContainerProps = {
   children?: React.ReactNode;
 } & Pick<LogoProps, "logoWidth" | "white">;
 
 const LogoStyledContainer = styled(Link)<ContainerProps>`
-  @import url("https://fonts.googleapis.com/css2?family=Signika:wght@600&display=swap");
   font-family: "Signika", sans-serif;
   font-weight: 600;
   text-decoration: none;
@@ -45,9 +42,11 @@ const LogoStyledContainer = styled(Link)<ContainerProps>`
     `}
 `;
 
-const LogoSignet = styled.span<LogoSignetProps>`
-  background: url(${({ white }) => white ? logoVersions.whiteURL : logoVersions.colorURL})
-    no-repeat center / contain;
+const LogoSignet = styled.span<LogoProps>`
+  background-image: url(${({ white }) => white && logoVersions.white});
+  background-image: url(${() => logoVersions.color});
+  background-repeat: no-repeat;
+  background-size: contain;
   height: 100%;
   ${({ logoWidth }) =>
     logoWidth &&
