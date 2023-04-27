f6437737e9b26651ef78dd0ee7c5708a865a8ac2
PawelPazderski <pazderski.projekt@gmail.com>
2023-04-27 08:12:25 +0200

    fix: delete redundant lines

--- apps/web/app/(navigation)/budgets/BudgetsTranslated.tsx
+++ apps/web/app/(navigation)/budgets/BudgetsTranslated.tsx
@@ -6,6 +6,8 @@ import MultiCardLayout from "../MultiCardLayout";
 import { BudgetBasicInformation } from './[id]/BudgetBasicInformation';
 import { Budget } from "lib/types";
 
+import { TransactionsTable } from "./[id]/TransactionsTable";
+
 const budget: Budget = {
   "id": "36cc0777-2874-4d0c-a389-91280e45e836",
   "name": "Trip to London",
@@ -18,22 +20,6 @@ const budget: Budget = {
   "startDate": 1677628800000,
   "endDate": 1678838400000,
   "limit": 20000
-};
-
-import { TransactionsTable } from "./[id]/TransactionsTable";
-
-const budget = {
-  id: "71ee9a04-6b27-423a-9caa-7d6a92335dae",
-  name: "Main budget",
-  description: "Main budget of me and my wife",
-  icon: "home",
-  currency: {
-    locale: "en-US",
-    tag: "USD",
-  },
-  startDate: 1680307200000,
-  endDate: 1682899200000,
-  limit: 10000,
 };
 
 export const BudgetsTitle = ({ name = "" }) => {
