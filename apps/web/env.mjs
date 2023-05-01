435fc5e5adb2c3c6b016baa3f272888b5148269c
Xewol <bartekdeluga0702@gmail.com>
2023-05-01 20:31:52 +0200

    refactor: change server env to client

--- apps/web/env.mjs
+++ apps/web/env.mjs
@@ -2,10 +2,12 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    API_URL: z.string().url(),
+  client: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
   },
+
+  // need to use [] because process.env works differently in turbo-land
   runtimeEnv: {
-    API_URL: process.env.API_URL,
+    NEXT_PUBLIC_API_URL: process.env["NEXT_PUBLIC_API_URL"],
   },
 });
