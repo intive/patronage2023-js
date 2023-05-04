c3834d003238beefc6d159fad4db004d5b163945
Bartosz Kędziora <bartosz.kedziora1@gmail.com>
2023-05-04 12:51:54 +0200

    fix: changed API_URL to client variable

--- apps/web/env.mjs
+++ apps/web/env.mjs
@@ -2,13 +2,13 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    API_URL: z.string().url(),
+  client: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
   },
 
   // need to use [] because process.env works in next js
   //https://stackoverflow.com/a/76105098
   runtimeEnv: {
-    API_URL: process.env["API_URL"],
+    NEXT_PUBLIC_API_URL: process.env["NEXT_PUBLIC_API_URL"],
   },
 });
