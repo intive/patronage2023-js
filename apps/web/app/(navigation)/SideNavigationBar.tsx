9c753eb2306fde5e03b6c22b4d0729579b2c7009
Daniel Chmur <daniel.chmur@gmail.com>
2023-05-06 09:34:13 +0200

    chore: added correct error catching in fetch

--- apps/web/app/(navigation)/SideNavigationBar.tsx
+++ apps/web/app/(navigation)/SideNavigationBar.tsx
@@ -37,8 +37,9 @@ export default function SideNav() {
   const url =
     "https://inbudget-patronage-api-dev.azurewebsites.net/budgets/list";
 
+  // const token = "dsf";
   const token =
-    "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJha0lYQnV6SHhGb1RINkgxRFNhTkRiVlk4MnBMWXRNdFdVMkRPTjNHTXNnIn0.eyJleHAiOjE2ODMyOTk1MzMsImlhdCI6MTY4MzI5MjMzMywianRpIjoiMjNlYTYwZGMtNjRjMy00YjQ2LWEyOWItZWZhODhjMDVlODMzIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay1pbmJ1ZGdldC1wYXRyb25hZ2UyMDIzLmF6dXJld2Vic2l0ZXMubmV0L3JlYWxtcy9pbmJ1ZGdldC1yZWFsbS1kZXYiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiZTE3MjUyYmEtMjc5ZS00NWM3LWJhMWItNjcwMDNkZWI2YzAzIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiaW5idWRnZXQtY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6IjdkNWE4OWQ0LWM3ZTAtNGI5ZS1hNzA1LTcxNzgxY2JjM2I2ZSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiLyoiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1pbmJ1ZGdldC1yZWFsbS1kZXYiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsInNpZCI6IjdkNWE4OWQ0LWM3ZTAtNGI5ZS1hNzA1LTcxNzgxY2JjM2I2ZSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6IkphbiBLb3dhbHNraSIsImF2YXRhciI6ImF2YXRhciIsInByZWZlcnJlZF91c2VybmFtZSI6Imprb3dhbHNraUBnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiSmFuIiwiZmFtaWx5X25hbWUiOiJLb3dhbHNraSIsImVtYWlsIjoiamtvd2Fsc2tpQGdtYWlsLmNvbSJ9.LmHOwKeY1uDPfWbCHt1Eqxa3lmWhOvJG1bDZDtwHD5qlZty81Vc8E99Aj-0tBRpTLAT8FCuU5RjLxwOBXVa6YD0HgwsxkT4ukOiHFbSnBezcDWOKtERyLERuEDmB7Totzv67u8GWf02cFjEpssYo2CX6xxLu2Z31uD4ys25TIp1TbzEPn-nvWKwzCP9Qq1JX_YS8eGTYQYpIITjuNxzAbsBzL6J5pWBeQLCwljEFt1Qm4afMr5z2nSx5w8evuwhWIuXFQTN44P5Tr__4SkacRRK64Vry3BQ46dkTFGt9MDX8GSvYXjsXOr92NLJ64Q4BYLTVMAG9-DOkgD1fQIYCHA";
+    "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJha0lYQnV6SHhGb1RINkgxRFNhTkRiVlk4MnBMWXRNdFdVMkRPTjNHTXNnIn0.eyJleHAiOjE2ODMzNjUwODIsImlhdCI6MTY4MzM1Nzg4MiwianRpIjoiMzViYTkyZDgtODg5Zi00OGQzLThlZjYtYjFjMjg4YjZlODVmIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay1pbmJ1ZGdldC1wYXRyb25hZ2UyMDIzLmF6dXJld2Vic2l0ZXMubmV0L3JlYWxtcy9pbmJ1ZGdldC1yZWFsbS1kZXYiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiZTE3MjUyYmEtMjc5ZS00NWM3LWJhMWItNjcwMDNkZWI2YzAzIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiaW5idWRnZXQtY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6IjNjNDE4ZjAwLTgzZTgtNDIzNy04MjU4LWIwMzJiN2U5ZDAyOCIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiLyoiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1pbmJ1ZGdldC1yZWFsbS1kZXYiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsInNpZCI6IjNjNDE4ZjAwLTgzZTgtNDIzNy04MjU4LWIwMzJiN2U5ZDAyOCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6IkphbiBLb3dhbHNraSIsImF2YXRhciI6ImF2YXRhciIsInByZWZlcnJlZF91c2VybmFtZSI6Imprb3dhbHNraUBnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiSmFuIiwiZmFtaWx5X25hbWUiOiJLb3dhbHNraSIsImVtYWlsIjoiamtvd2Fsc2tpQGdtYWlsLmNvbSJ9.w5yyaoebt-jz3sYTfro4RShcqkAikLDA3smH6BmLyoIk_hDUvSTsEyT6euF8yCckmtCP5kkNoHCsHr5dN2c4llXfOj4MHfuSXEANqK6WJdUYWNrQ-kKh_bR3alBHHGduYLNSnVTVN1dS_xI_KMtl0ShPSvG9ad4VTB3X0R8MPbuo6rqn5q6A9X7M0WyNCb99OJN8ViPwFacZM5SbBnKT2Y1ebXR3lT0RSQxFmKqFQe8DoEpIQjyKQDrh2VaAnAi9K6AQfPER57y7sn4Xdz1ZxTn_GFahwgLk8r4xry24Z_h4aM4Op5VRMuwNEAE8KB9DcJaCl77rqnY2GmiM0aQOrQ";
 
   const getBudgetsList = async (
     url: string,
@@ -64,11 +65,14 @@ export default function SideNav() {
         ],
       }),
     })
-      .then((res) => res.json())
-      .then((json) => {
-        setData(json.items);
-        console.log(data);
-      });
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then((data) => setData(data.items))
+      .catch((err) => console.error(err));
   };
 
   useEffect(() => {
