22c7134e7ba1ee491584562d1714121d92696eb9
Xewol <bartekdeluga0702@gmail.com>
2023-05-06 12:06:05 +0200

    feat: temporary changes to get data from backend

--- apps/web/app/(navigation)/budgets/BudgetsContent.tsx
+++ apps/web/app/(navigation)/budgets/BudgetsContent.tsx
@@ -2,13 +2,11 @@
 
 import { DummyAsideCardContent } from "app/DummyAsideCardContent";
 import MultiCardLayout from "../MultiCardLayout";
-
+import { useQuery } from "@tanstack/react-query";
 import { TransactionsTable } from "./[id]/TransactionsTable";
 import { BudgetBasicInformation } from "./[id]/BudgetBasicInformation";
-import { Budget } from "lib/types";
-import { useEffect, useMemo, useState } from "react";
 import styled from "styled-components";
-import { useParams } from "next/navigation";
+import { env } from "env.mjs";
 
 const BudgetContentWrapperStyled = styled.div`
   display: flex;
@@ -17,36 +15,39 @@ const BudgetContentWrapperStyled = styled.div`
   align-items: center;
   align-self: flex-start;
   gap: 32px;
+  width: 100%;
 `;
 
+interface BudgetsContentProps {
+  id: string;
+}
+
 export const BudgetsContent = () => {
-  const params = useParams();
+  const id = "3e9ca5f0-5ef8-44bc-a8bc-175c826b39b5";
 
-  const id = params?.id as string;
+  const token = `eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJha0lYQnV6SHhGb1RINkgxRFNhTkRiVlk4MnBMWXRNdFdVMkRPTjNHTXNnIn0.eyJleHAiOjE2ODMzNjg0OTYsImlhdCI6MTY4MzM2MTI5NiwianRpIjoiOGRlMjNkMzktNmMxMy00YTkyLTk2MDktZmE4ZmIyMWY4MzAzIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay1pbmJ1ZGdldC1wYXRyb25hZ2UyMDIzLmF6dXJld2Vic2l0ZXMubmV0L3JlYWxtcy9pbmJ1ZGdldC1yZWFsbS1kZXYiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiODIwNjNmMmUtOWQ5YS00YjM4LWEyMmUtNTU3MmNlZTlkZGY0IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiaW5idWRnZXQtY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6IjdkY2JhYmYwLTJkZmItNGRlNi05OTVlLTI0MjVkZmJjZmYxMSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiLyoiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1pbmJ1ZGdldC1yZWFsbS1kZXYiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsInNpZCI6IjdkY2JhYmYwLTJkZmItNGRlNi05OTVlLTI0MjVkZmJjZmYxMSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYW1lIjoic211dG5hIHphYmEiLCJhdmF0YXIiOiIxIiwicHJlZmVycmVkX3VzZXJuYW1lIjoic211dG5hcnphYmFAcG5nLnBsIiwiZ2l2ZW5fbmFtZSI6InNtdXRuYSIsImZhbWlseV9uYW1lIjoiemFiYSIsImVtYWlsIjoic211dG5hcnphYmFAcG5nLnBsIn0.GuoHDB4S8yK870MCxrlmMF2sU01fvY7mf2hvbG8Q0jD2pWWDZmaBgw_kIC4td0J4wWaTwRfNPQ3EAAAFOtDLKw5tRRxfMMil71Eq2sXRTS6dpnAHtpSvvVaizyx4jO_nd5ll2hKbNeNvtEldPIhVHaNdxKfThJ6Ydxat2VzNpQqn9k3wuZwndd4TdWgdlnQsbypXwWWSKDYvGtfoLMQCjPJ6XbHsWMmUVodZoFMuCxoKpnCCn3ccJJYvn-cLjIOUKsIROiodVRHbADjZ6adCg1_zxrP6KIWNkybNwyYN07rF-tjL7H4wYUMCvVpmw05ZiYGQdtz-b52QyhcftOIBjg`;
 
-  const [budgets, setBudgets] = useState<Budget[]>([]);
-
-  useEffect(() => {
-    fetch(`/budgets.json`)
-      .then((response) => response.json())
-      .then((result) => {
-        setBudgets(result.budgets);
-        console.log(result);
-      });
-  }, []);
-
-  const budget = useMemo(
-    () => budgets.find((currentBudget) => currentBudget.id === id),
-    [id, budgets]
-  );
-  console.log(budget);
-  const mainCardContent = budget && (
+  const { data: budget } = useQuery({
+    queryKey: ["budgets", id],
+    queryFn: async () => {
+      return fetch(`${env.NEXT_PUBLIC_API_URL}/budgets/${id}`, {
+        headers: {
+          accept: "*/*",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      }).then((res) => res.json());
+    },
+  });
+  const mainCardContent = (
     <BudgetContentWrapperStyled>
       <BudgetBasicInformation budget={budget} />
-      <TransactionsTable
-        budget={budget}
-        setSorting={(column) => console.log(column)}
-      />
+      {budget && (
+        <TransactionsTable
+          budget={budget}
+          setSorting={(column) => console.log(column)}
+        />
+      )}
     </BudgetContentWrapperStyled>
   );
 
