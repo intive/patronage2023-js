83a2f3022181f33e94284b097814a2c9f1088aac
Xewol <bartekdeluga0702@gmail.com>
2023-05-10 21:04:04 +0200

    feat: working on component

--- apps/web/app/(navigation)/budgets/[id]/BudgetStatistics.tsx
+++ apps/web/app/(navigation)/budgets/[id]/BudgetStatistics.tsx
@@ -1,12 +1,27 @@
 import { type BudgetGeneralInfo } from "lib/types";
 import React from "react";
-
+import styled from "styled-components";
+import { CurrencyAmount } from "ui";
 interface Props {
   budget: BudgetGeneralInfo;
+  value:number
 }
 
+const DetailsWrapperStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+
+
+
 const BudgetStatistics = ({ budget }: Props) => {
-  return <div>{JSON.stringify(budget)}</div>;
+  return (
+    <DetailsWrapperStyled>
+      <div></div>
+      <CurrencyAmount amount={budget.}>
+    </DetailsWrapperStyled>
+  );
 };
 
 export default BudgetStatistics;
