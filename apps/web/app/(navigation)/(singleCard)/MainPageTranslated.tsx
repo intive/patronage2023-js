378614248e778a306b979eb3ac0fe146fc6239cd
Bartosz Kędziora <bartosz.kedziora1@gmail.com>
2023-05-10 18:03:01 +0200

    fix: Small refactor on main page

--- apps/web/app/(navigation)/(singleCard)/MainPageTranslated.tsx
+++ apps/web/app/(navigation)/(singleCard)/MainPageTranslated.tsx
@@ -2,7 +2,7 @@
 
 import { TypoStyled, LinkStyled } from "app/(navigation)/HomePageComponents";
 import { useTranslate } from "lib/hooks";
-import { signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "ui";
 import styled from "styled-components";
 
@@ -20,18 +20,19 @@ export const MainPageTranslated = () => {
     <ContentWrapperStyled>
       <TypoStyled>{t(dict.welcomeText)}</TypoStyled>
       <LinkStyled href="/sign-up">{t(dict.createAccountLink)}</LinkStyled>
-      <LinkStyled href="/sign-in">Sign in</LinkStyled>
-      <br />
-      <Button onClick={() => signOut()}>Log out</Button>
       <br />
       {data ? (
-        <p>
-          User: {data.user.name}, <br /> Avatar: {data.user.image}, <br />{" "}
-          AccessToken:
-          {data.user.accessToken.substring(0, 30)}...
-        </p>
+        <>
+          <Button onClick={() => signOut()}>Log out</Button>
+          <br />
+          <p>
+            User: {data.user.name}, <br /> Avatar: {data.user.image}, <br />{" "}
+            AccessToken:
+            {data.user.accessToken.substring(0, 30)}...
+          </p>
+        </>
       ) : (
-        <p>Please log in</p>
+        <Button onClick={() => signIn()}>Sign in</Button>
       )}
     </ContentWrapperStyled>
   );
