92487e9f9d8ee16590a5658d2d432d7dcc3ba445
Xewol <bartekdeluga0702@gmail.com>
2023-05-11 18:50:43 +0200

    fix: pcham

--- packages/ui/Spinner/index.tsx
+++ packages/ui/Spinner/index.tsx
@@ -1,7 +1,8 @@
+import { useContext } from "react";
 import { SyncLoader } from "react-spinners";
-import { useTheme } from "styled-components";
+import { ThemeContext, useTheme } from "styled-components";
 
 export const Spinner = () => {
-  const theme = useTheme();
-  return <SyncLoader color={theme.primary as any} />;
+  const theme = useContext(ThemeContext);
+  return <SyncLoader color={theme.primary} />;
 };
