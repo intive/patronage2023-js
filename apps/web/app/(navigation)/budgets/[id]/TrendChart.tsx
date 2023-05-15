50fdb3e3832a3dd4a2c033603b43476f44b10dd2
wy_ola <80450315+wyola@users.noreply.github.com>
2023-05-15 15:25:05 +0200

    ref: changes from PR, rename variables, typos, use theme #P23-330

--- apps/web/app/(navigation)/budgets/[id]/TrendChart.tsx
+++ apps/web/app/(navigation)/budgets/[id]/TrendChart.tsx
@@ -45,78 +45,77 @@ type TrendChartProps = {
     locale: string;
   };
 };
-// code to generete color for line - red or green based on value
-let width = 0,
-  height = 0,
-  gradient: any;
 
-function getGradient(
-  ctx: CanvasRenderingContext2D,
-  chartArea: ChartArea,
-  min: number,
-  max: number
-) {
-  const chartWidth = chartArea.right - chartArea.left;
-  const chartHeight = chartArea.bottom - chartArea.top;
-
-  const green = "#92CE78";
-  const red = "#E57070";
-  // all values are > 0
-  if (min > 0) {
-    return green;
-  }
-
-  // all values < 0
-  if (max < 0) {
-    return red;
-  }
-
-  if (min === 0 && max === 0) {
-    return green;
-  }
-
-  if (!gradient || width !== chartWidth || height !== chartHeight) {
-    // Create the gradient because this is either the first render
-    // or the size of the chart has changed
-    width = chartWidth;
-    height = chartHeight;
-
-    // 0.5 is in the middle of the chart
-    const minWithMax = max + Math.abs(min);
-    const zeroPoint = Math.abs(min) / minWithMax;
-
-    gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
-
-    gradient.addColorStop(0, red);
-    gradient.addColorStop(zeroPoint - 0.00001, red);
-    gradient.addColorStop(zeroPoint, green);
-    gradient.addColorStop(1, green);
-  }
-  return gradient;
-}
 export const TrendChart = ({ statistics, currency }: TrendChartProps) => {
   const theme = useContext(ThemeContext);
   const { t, dict } = useTranslate("BudgetsPage");
   const { charts } = dict;
 
   const sortedData = statistics?.items.sort((a, b) => {
-    const aTimestap = new Date(a.datePoint).getTime();
-    const bTimestap = new Date(b.datePoint).getTime();
-    return aTimestap - bTimestap;
+    const aTimestamp = new Date(a.datePoint).getTime();
+    const bTimestamp = new Date(b.datePoint).getTime();
+    return aTimestamp - bTimestamp;
   });
 
   if (!sortedData) {
     return null;
   }
 
-  const dates = [];
-  const values = [];
-  for (let item of sortedData) {
-    dates.push(item.datePoint);
-    values.push(item.value);
+  const dates = sortedData.map((data) => data.datePoint);
+  const values = sortedData.map((data) => data.value);
+
+  const minValue = Math.min(...values);
+  const maxValue = Math.max(...values);
+
+  // code to generete color for line
+  let width = 0,
+    height = 0,
+    gradient: CanvasGradient;
+
+  function getGradient(
+    ctx: CanvasRenderingContext2D,
+    chartArea: ChartArea,
+    minValue: number,
+    maxValue: number
+  ) {
+    const chartWidth = chartArea.right - chartArea.left;
+    const chartHeight = chartArea.bottom - chartArea.top;
+
+    const positiveValueColor = theme.trendChart.positiveLine;
+    const negativeValueColor = theme.trendChart.negativeLine;
+
+    if (minValue > 0) {
+      return positiveValueColor;
+    }
+    if (maxValue < 0) {
+      return negativeValueColor;
+    }
+    if (minValue === 0 && maxValue === 0) {
+      return positiveValueColor;
+    }
+
+    if (!gradient || width !== chartWidth || height !== chartHeight) {
+      width = chartWidth;
+      height = chartHeight;
+
+      const minWithMax = maxValue + Math.abs(minValue);
+      const zeroPoint = Math.abs(minValue) / minWithMax;
+
+      gradient = ctx.createLinearGradient(
+        0,
+        chartArea.bottom,
+        0,
+        chartArea.top
+      );
+
+      gradient.addColorStop(0, negativeValueColor);
+      gradient.addColorStop(zeroPoint - 0.00001, negativeValueColor);
+      gradient.addColorStop(zeroPoint, positiveValueColor);
+      gradient.addColorStop(1, positiveValueColor);
+    }
+    return gradient;
   }
-  const min = Math.min(...values);
-  const max = Math.max(...values);
+
   return (
     <StyledWrapper>
       <StyledTitle>{t(charts.titleLeft)}</StyledTitle>
@@ -139,8 +138,8 @@ export const TrendChart = ({ statistics, currency }: TrendChartProps) => {
                   if (!chartArea) {
                     return;
                   }
-                  return getGradient(ctx, chartArea, min, max);
-                }, // TODO: line color for negative ???
+                  return getGradient(ctx, chartArea, minValue, maxValue);
+                },
                 borderWidth: 2,
                 fill: {
                   target: "origin",
