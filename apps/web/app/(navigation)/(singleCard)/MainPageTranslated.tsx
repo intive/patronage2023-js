3a4d1866cbd9eceeaae39ba819b151d6fc4f5750
Xewol <bartekdeluga0702@gmail.com>
2023-05-15 16:59:40 +0200

    fix: another dynamically rendered styled component

--- apps/web/app/(navigation)/(singleCard)/MainPageTranslated.tsx
+++ apps/web/app/(navigation)/(singleCard)/MainPageTranslated.tsx
@@ -6,15 +6,14 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "ui";
 import styled from "styled-components";
 
+const ContentWrapperStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 export const MainPageTranslated = () => {
   const { dict, t } = useTranslate("MainPage");
   const { data } = useSession();
-
-  const ContentWrapperStyled = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  `;
 
   return (
     <ContentWrapperStyled>
