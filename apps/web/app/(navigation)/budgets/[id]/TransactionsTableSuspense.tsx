2d9512cedcb1497be7f74548fa6b7aca57c261f5
Xewol <bartekdeluga0702@gmail.com>
2023-05-15 16:57:37 +0200

    fix: fixed warnings in console about rendering styled components dynamically

--- apps/web/app/(navigation)/budgets/[id]/TransactionsTableSuspense.tsx
+++ apps/web/app/(navigation)/budgets/[id]/TransactionsTableSuspense.tsx
@@ -2,46 +2,46 @@ import { SkeletonLoading } from "ui";
 import styled from "styled-components";
 import { PropsWithChildren } from "react";
 
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  gap: 20px;
+  flex-basis: 100%;
+  width: 100%;
+`;
+
+const HeadersWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 30%;
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-start;
+  width: 100%;
+  flex-basis: 70%;
+  gap: 10px;
+  position: relative;
+`;
+
+const Gradient = styled.div`
+  background: linear-gradient(to bottom, transparent, white);
+  z-index: 1;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+`;
 export const TransactionsTableSuspense = () => {
-  const MainWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    gap: 20px;
-    flex-basis: 100%;
-    width: 100%;
-  `;
-
-  const HeadersWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 30%;
-  `;
-
-  const ContentWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: flex-start;
-    width: 100%;
-    flex-basis: 70%;
-    gap: 10px;
-    position:relative;
-  `;
-
-  const Gradient = styled.div`
-    background: linear-gradient(to bottom,transparent, white);
-    z-index:1;
-    position:absolute;
-    left:0;
-    bottom:0;
-    width:100%;
-    height:100%;
-  `
   const SingleSkeletonWrapperHeader = ({
     children,
   }: PropsWithChildren<unknown>) => {
@@ -62,7 +62,7 @@ export const TransactionsTableSuspense = () => {
       <div
         style={{
           width: "100%",
-          zIndex:0,
+          zIndex: 0,
         }}>
         {children}
       </div>
@@ -72,19 +72,19 @@ export const TransactionsTableSuspense = () => {
   return (
     <MainWrapper>
       <HeadersWrapper>
-      <SkeletonLoading
-        count={4}
-        containerClassName="skeleton-header-wrapper"
-        wrapper={SingleSkeletonWrapperHeader}
-      /> 
+        <SkeletonLoading
+          count={4}
+          containerClassName="skeleton-header-wrapper"
+          wrapper={SingleSkeletonWrapperHeader}
+        />
       </HeadersWrapper>
       <ContentWrapper>
-      < Gradient/>
-      <SkeletonLoading
-        count={4}
-        containerClassName="flex-content-wrapper"
-        wrapper={SingleSkeletonWrapperContent}
-      />
+        <Gradient />
+        <SkeletonLoading
+          count={4}
+          containerClassName="flex-content-wrapper"
+          wrapper={SingleSkeletonWrapperContent}
+        />
       </ContentWrapper>
     </MainWrapper>
   );
