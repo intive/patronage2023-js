8104a8b120018e6a5225d9f78c6f9893e03da66b
Bartosz Kędziora <bartosz.kedziora1@gmail.com>
2023-05-15 21:47:17 +0200

    feat: added styles and error handling

--- apps/web/app/(navigation)/budgets/[id]/RemoveBudget.tsx
+++ apps/web/app/(navigation)/budgets/[id]/RemoveBudget.tsx
@@ -1,18 +1,27 @@
 import { BudgetFixed } from "lib/types";
-import { Modal } from "ui";
+import { Button, ErrorMessage, Modal } from "ui";
 import { useTranslate } from "lib/hooks";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { env } from "env.mjs";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import styled from "styled-components";
 
 interface RemoveBudgetProps {
   budget: BudgetFixed;
   onClose: () => void;
 }
 
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+  padding: 0 0 10px 0;
+`;
+
 export const RemoveBudget = ({ budget, onClose }: RemoveBudgetProps) => {
   const { t, dict } = useTranslate("BudgetsPage");
+  const [toggleErrorBox, setToggleErrorBox] = useState<string | unknown>("");
   const queryClient = useQueryClient();
   const { data: session } = useSession();
   const { replace } = useRouter();
@@ -40,17 +49,36 @@ export const RemoveBudget = ({ budget, onClose }: RemoveBudgetProps) => {
         .then((r) => console.log(r));
       replace("/");
     },
+    onError: (error) => {
+      setToggleErrorBox(error);
+    },
   });
 
   return (
     <>
       <Modal onClose={onClose} header={t(dict.removeBudgetModal.header)}>
-        <button onClick={() => deleteBudget.mutate(budget.id)}>
-          {t(dict.removeBudgetModal.confirmButton)}
-        </button>
-        <button onClick={onClose}>
-          {t(dict.removeBudgetModal.abortButton)}
-        </button>
+        <>
+          {!toggleErrorBox && (
+            <ButtonWrapper>
+              <Button onClick={() => deleteBudget.mutate(budget.id)}>
+                {t(dict.removeBudgetModal.confirmButton)}
+              </Button>
+              <Button onClick={onClose} variant={"secondary"}>
+                {t(dict.removeBudgetModal.abortButton)}
+              </Button>
+            </ButtonWrapper>
+          )}
+
+          {toggleErrorBox && (
+            <ErrorMessage
+              message={`${toggleErrorBox}`}
+              onClose={() => {
+                setToggleErrorBox("");
+                onClose();
+              }}
+            />
+          )}
+        </>
       </Modal>
     </>
   );
