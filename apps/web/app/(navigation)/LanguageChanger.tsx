c036e1406b9904644345496535c237a3142e2848
Daniel Chmur <daniel.chmur@gmail.com>
2023-05-16 18:55:53 +0200

    chore: working with radix select

--- apps/web/app/(navigation)/LanguageChanger.tsx
+++ apps/web/app/(navigation)/LanguageChanger.tsx
@@ -1,40 +1,85 @@
-import { LanguageContext, languages } from "lib/contexts";
-import { useContext } from "react";
-import styled from "styled-components";
-import { Flag, Button } from "ui";
+import { languages } from "lib/contexts";
+import * as Select from "@radix-ui/react-select";
+
+import { Flag } from "ui";
+import { ContentStyled } from "./LanguageChangerStyled";
+import { useState } from "react";
 
 export const LanguageChanger = () => {
-  const { currentLang, setLang } = useContext(LanguageContext); // will I neede it?
+  const [language, setLanguage] = useState(localStorage.getItem("lang"));
 
-  const setLanguage = (lang: languages) => {
+  const changeLanguage = (lang: string) => {
+    setLanguage(lang);
     localStorage.setItem("lang", lang);
     window.location.reload();
   };
 
-  const ButtonStyled = styled(Button)`
-    img:last-child {
-      margin: -10px;
-    }
-  `;
+  const items = [
+    {
+      flagSrc: "/flags/pl.svg",
+      languageName: "Polish",
+    },
+    {
+      flagSrc: "/flags/en.svg",
+      languageName: "English",
+    },
+    {
+      flagSrc: "/flags/fr.svg",
+      languageName: "French",
+    },
+  ];
 
   return (
-    // change theese buttons to dropdown menu // types not added
-    <>
-      <ButtonStyled
-        variant={"secondary"}
-        onClick={() => setLanguage(languages.pl)}>
-        <Flag src="/flags/pl.svg" />
-      </ButtonStyled>
-      <ButtonStyled
-        variant={"secondary"}
-        onClick={() => setLanguage(languages.fr)}>
-        <Flag src="/flags/fr.svg" />
-      </ButtonStyled>
-      <ButtonStyled
-        variant={"secondary"}
-        onClick={() => setLanguage(languages.en)}>
-        <Flag src="/flags/gb.svg" />
-      </ButtonStyled>
-    </>
+    <Select.Root
+      value={(language as languages) || "en"}
+      onValueChange={(lang) => {
+        changeLanguage(lang as languages);
+      }}>
+      <Select.Trigger>
+        <Select.Value>{<Flag src={`/flags/${language}.svg`} />}</Select.Value>
+      </Select.Trigger>
+
+      <Select.Portal>
+        <ContentStyled position="popper" sideOffset={5}>
+          <Select.Viewport>
+            <Select.Item value={"fr"} onClick={() => changeLanguage("fr")}>
+              <Flag src="/flags/fr.svg" />
+              <Select.ItemText>French</Select.ItemText>
+            </Select.Item>
+            <Select.Item value={"pl"} onClick={() => changeLanguage("pl")}>
+              Polish
+              <Select.ItemText> fdgdg</Select.ItemText>
+            </Select.Item>
+
+            <Select.Separator />
+          </Select.Viewport>
+          <Select.ScrollDownButton />
+          <Select.Arrow />
+        </ContentStyled>
+      </Select.Portal>
+    </Select.Root>
+    // <Select.Root value={language}>
+    //   <Select.Trigger asChild>
+    //     <Select.Value placeholder="test" />
+    //   </Select.Trigger>
+
+    //   <Select.Portal>
+    //     <Select.Content position="popper">
+    //       <Select.Viewport>
+    //         <Select.Group>
+    //           {items.map((item) => (
+    //             <Select.Item
+    //               onClick={item.onClick}
+    //               key={item.languageName}
+    //               value={item.languageName}>
+    //               <Flag src={item.flagSrc} />
+    //               <Select.ItemText>{item.languageName}</Select.ItemText>
+    //             </Select.Item>
+    //           ))}
+    //         </Select.Group>
+    //       </Select.Viewport>
+    //     </Select.Content>
+    //   </Select.Portal>
+    // </Select.Root>
   );
 };
---
+++ apps/web/app/(navigation)/LanguageChangerStyled.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import * as Select from "@radix-ui/react-select";
+
+export const ContentStyled = styled(Select.Content)`
+  color: black;
+  background-color: white;
+  z-index: 40000;
+`;
