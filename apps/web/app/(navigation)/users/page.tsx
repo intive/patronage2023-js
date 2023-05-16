12f3f0a3b9475d1ca7df5fe46c1d0ec649d13427
Xewol <bartekdeluga0702@gmail.com>
2023-05-16 11:29:15 +0200

    feat: styled UserPage

--- apps/web/app/(navigation)/users/page.tsx
+++ apps/web/app/(navigation)/users/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+import styled from "styled-components";
 import MultiCardLayout from "../MultiCardLayout";
 
-export default function UsersPage() {
-  const mainCardContent = <h1>Users</h1>;
+const TitleStyled = styled.h1`
+  align-self: flex-start;
+  width: 100%;
+  font-family: "Signika", sans-serif;
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 48px;
+  color: ${({ theme }) => theme.main};
+`;
 
-  //pass to data aside when clicked on user
+export default function UsersPage() {
+  const mainCardContent = <TitleStyled>Users</TitleStyled>;
+
   return <MultiCardLayout main={mainCardContent} />;
 }
