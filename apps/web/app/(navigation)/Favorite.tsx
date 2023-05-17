e484af86668454d32c95753139fb819d0191ffd8
ZuzannaPaw <zuzannapaw@wp.pl>
2023-05-17 11:03:51 +0200

    refactor: basic solution #P23-36

--- apps/web/app/(navigation)/Favorite.tsx
+++ apps/web/app/(navigation)/Favorite.tsx
@@ -2,7 +2,7 @@
 
 import { BudgetType } from "../../services/mutations";
 import { Icon } from "ui";
-import { SyntheticEvent, useState } from "react";
+import {  useState } from "react";
 import styled from "styled-components";
 import { usePathname } from "next/navigation";
 
@@ -24,40 +24,57 @@ const Wrapper = styled.div<Active>`
   margin-left: 50px;
   z-index: 1;
 `;
+
 let favBudgets: Array<any> = [];
+let ariaLabel;
+
 export const Favorite = ({
-  ariaLabel,
   budgets,
   isFav,
   budgetId,
   activeHref,
 }: FavoriteProps) => {
   const currentPage = usePathname() || "";
-  //in use state it will be what will come from prop?
-  const [isFavorite, setIsFavorite] = useState(isFav);
 
+  //in use state it will be what will come from prop... I think not, because true or false will come from each of items from fetch. When add to fav/remove from fav, fetch will fire, what will cause change of props, render component and actual data. 
+  //initial state is what will come from prop
+  const [isFavorite, setIsFavorite] = useState(isFav);
+  ariaLabel = isFavorite === false? "Add to favorites" : "Remove from favorites"
   const active = currentPage === activeHref;
- 
-  const setFav = (event) => {
-    event.preventDefault()
-    event.stopPropagation()
+
+  const setFavHandler = (event:React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
     setIsFavorite((prevState) => !prevState);
-    const chosenBudget = budgets.find((budget) => budget.id.value === budgetId);
-//splice function must be added. NOT UNSHIFT
-    isFav === false
-      ? favBudgets.push(chosenBudget)
-      : favBudgets.unshift(chosenBudget);
-    console.log(` YOUR FAV BUDGETS! : ${JSON.stringify(favBudgets)}`);
-    return
+
+
+    if (isFavorite === false) {
+      const chosenBudget = budgets.find(
+        (budget) => budget.id.value === budgetId
+      );
+      favBudgets.push(chosenBudget);
+
+      console.log(`✨ You added to favorites: ${JSON.stringify(chosenBudget)}`);
+      ariaLabel= "Remove from favorites"
+    } else {
+      const favIndexToRemove = favBudgets.findIndex(
+        (favBudget) => favBudget.id === budgetId
+      );
+     const deletetBudget = favBudgets.splice(favIndexToRemove, 1);
+     console.log(`💥 You removed from favorites: ${JSON.stringify(deletetBudget)}`);
+      ariaLabel="Add to favorites"
+    }
+    console.log(`❤️ Your fav budgets! : ${JSON.stringify(favBudgets)}`);
+    return;
   };
 
   //filled isFav = if true, filled, if false, outlined
   return (
-    <Wrapper onClick={setFav} active={active}>
+    <Wrapper onClick={setFavHandler} active={active} aria-label={ariaLabel}>
       <Icon
         icon="favorite"
         filled={isFavorite}
-        color="#B1B1B1"
+        color="#ef4e4e"
         iconSize={20}></Icon>
     </Wrapper>
   );
