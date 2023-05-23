533dbd0dc573ed7790aaabf1bc18fd676ae75aec
ZuzannaPaw <zuzannapaw@wp.pl>
2023-05-23 12:22:27 +0200

    refactor: changes for upcoming endpoint #P23-36

--- apps/web/app/(navigation)/Favorite.tsx
+++ apps/web/app/(navigation)/Favorite.tsx
@@ -2,7 +2,7 @@
 
 import { BudgetType } from "../../services/mutations";
 import { Icon } from "ui";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
@@ -37,16 +37,14 @@ export const Favorite = ({
   const currentPage = usePathname() || "";
   const { data: session } = useSession();
 
-  //in use state it will be what will come from prop... I think not, because true or false will come from each of items from fetch. When add to fav/remove from fav, fetch will fire, what will cause change of props, render component and actual data.
   //initial state is what will come from prop
   const [isFavorite, setIsFavorite] = useState(isFav);
   ariaLabel = isFavorite ? "Remove from favorites" : "Add to favorites";
   const active = currentPage === activeHref;
 
-  //function for modify isFav
-  const changeFavHandler = async (isFav: boolean) => {
-    const response = await fetch(
-      `https://inbudget-patronage-api-dev.azurewebsites.net/budgets/${budgetId}/favourite?isFavourite=${isFav}`,
+  useEffect(() => {
+    fetch(
+      `https://inbudget-patronage-api-dev.azurewebsites.net/budgets/${budgetId}/favourite?isFavourite=${isFavorite}`,
       {
         method: "PUT",
         headers: {
@@ -55,13 +53,28 @@ export const Favorite = ({
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then((response) => {
+      if (response.status === 400) {
+        setIsFavorite((prevState) => !prevState);
+      }
+    });
+  }, [isFavorite]);
 
-    // const data = await response.json() (no body yet)
-    //so the state will change on the previous one 
-    //if !response.ok =>  setIsFavorite((prevState) => prevState);
-    console.log(response);
-  };
+  //function for modify isFav
+  // const changeFavHandler = async (isFav: boolean) => {
+  //   const response = await fetch(
+  //     `https://inbudget-patronage-api-dev.azurewebsites.net/budgets/${budgetId}/favourite?isFavourite=${isFav}`,
+  //     {
+  //       method: "PUT",
+  //       headers: {
+  //         accept: "text/plain",
+  //         Authorization: "Bearer " + session!.user.accessToken,
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+
+  // };
 
   const setFavHandler = (event: React.MouseEvent<HTMLElement>) => {
     //to prevent relocating
@@ -70,11 +83,7 @@ export const Favorite = ({
     event.stopPropagation();
 
     setIsFavorite((prevState) => !prevState);
-
-    changeFavHandler(isFavorite);
-    // console.log(`✨ You added to favorites: ${JSON.stringify(chosenBudget)}`);
-   
-    // console.log( `💥 You removed from favorites: ${JSON.stringify(deletedBudget)}`)
+    console.log(isFavorite);
   };
 
   //filled isFav = if true, filled, if false, outlined
