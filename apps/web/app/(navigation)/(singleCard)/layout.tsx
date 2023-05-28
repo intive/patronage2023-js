9068b32b4e80a3775824819b7c24da5df2a433ea
Xewol <bartekdeluga0702@gmail.com>
2023-05-28 17:39:29 +0200

    refactor: updated imports

--- apps/web/app/(navigation)/(singleCard)/layout.tsx
+++ apps/web/app/(navigation)/(singleCard)/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CardWrapperStyled, CardStyled } from "components/HomePageComponents";
+import { CardWrapperStyled, CardStyled } from "components/HomePage.styled";
 import { LayoutProps } from "app/layout";
 
 export default function SingleCardLayout({ children }: LayoutProps) {
