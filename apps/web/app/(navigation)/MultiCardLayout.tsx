8797eecb322d79d2dacb5c0a6ce8b529e2fa030a
Xewol <bartekdeluga0702@gmail.com>
2023-05-28 17:40:06 +0200

    refactor: unused imports

--- apps/web/app/(navigation)/MultiCardLayout.tsx
+++ apps/web/app/(navigation)/MultiCardLayout.tsx
@@ -2,34 +2,15 @@
 import {
   CardWrapperStyled,
   CardStyled as MainCard,
-} from "components/HomePageComponents";
-import { device } from "lib/media-queries";
-import styled from "styled-components";
+  AsideCard,
+} from "components/HomePage.styled";
+
 import { ReactElement } from "react";
-import { Card } from "ui";
 
 type MultiCardLayoutProps = {
   main: ReactElement;
   aside?: ReactElement;
 };
-
-const AsideCard = styled(Card)`
-  display: none;
-  ${device.desktop} {
-    &:empty {
-      visibility: hidden;
-    }
-    display: flex;
-    flex-direction: column;
-    align-self: flex-start;
-    width: max-content;
-    min-width: 288px;
-    margin-left: 31px;
-    padding: 32px;
-    position: sticky;
-    top: 94px;
-  }
-`;
 
 export default function MultiCardLayout({ main, aside }: MultiCardLayoutProps) {
   return (
