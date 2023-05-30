8c2605b342af73dfc240b26fbc09d487b7720310
Anna Kozik <anna.kozik@yahoo.pl>
2023-05-30 15:11:24 +0200

    feat: try to add infinitescroll #P23-318

--- apps/web/app/(navigation)/ShareBudget.tsx
+++ apps/web/app/(navigation)/ShareBudget.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { env } from "env.mjs";
 import { useDebounce } from "lib/hooks/useDebounce";
 import { SearchInput } from "ui/Input/SearchInput";
@@ -13,6 +13,7 @@ import {
   ShareBudgetWrapperStyled,
   UsersListStyled,
 } from "./ShareBudget.styled";
+import { useGetUsers, ItemType } from "lib/hooks/useGetUsers";
 
 type User = {
   id: string;
@@ -34,6 +35,7 @@ type UsersListItemProps = {
   user: User;
   onCheckboxChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   checked: boolean;
+  ref?: (user: HTMLLIElement) => void;
 };
 
 const avatars = [
@@ -46,11 +48,6 @@ const avatars = [
   "/avatars/7.svg",
   "/avatars/8.svg",
 ];
-
-// type APIResponse = {
-//   items: User[];
-//   totalCount: number;
-// };
 
 const UsersListItem = ({
   user,
@@ -101,95 +98,127 @@ export const ShareBudget = ({
 
   const isChecked = (user: User) => budgetUsers.includes(user.id);
 
-  const mockUsers = [
-    {
-      firstName: "Howard",
-      lastName: "Wolovitz",
-      avatar: "/avatars/1.svg",
-      id: "1",
-    },
-    {
-      firstName: "Anna",
-      lastName: "Kozik",
-      avatar: "/avatars/2.svg",
-      id: "2",
-    },
-    {
-      firstName: "Jan",
-      lastName: "Kowalski",
-      avatar: "/avatars/3.svg",
-      id: "3",
-    },
-    {
-      firstName: "Julia",
-      lastName: "Nowak",
-      avatar: "/avatars/4.svg",
-      id: "4",
-    },
-  ];
-  const { data: session } = useSession();
+  // const mockUsers = [
+  //   {
+  //     firstName: "Howard",
+  //     lastName: "Wolovitz",
+  //     avatar: "/avatars/1.svg",
+  //     id: "1",
+  //   },
+  //   {
+  //     firstName: "Anna",
+  //     lastName: "Kozik",
+  //     avatar: "/avatars/2.svg",
+  //     id: "2",
+  //   },
+  //   {
+  //     firstName: "Jan",
+  //     lastName: "Kowalski",
+  //     avatar: "/avatars/3.svg",
+  //     id: "3",
+  //   },
+  //   {
+  //     firstName: "Julia",
+  //     lastName: "Nowak",
+  //     avatar: "/avatars/4.svg",
+  //     id: "4",
+  //   },
+  // ];
+  // const { data: session } = useSession();
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(500);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [itemsPerPage, setItemsPerPage] = useState(500);
   // const [totalPages, setTotalPages] = useState(1);
   const [searchValue, setSearchValue] = useState("");
   const debouncedSearch = useDebounce(searchValue, 500);
 
-  const {
-    data: allUsers,
-    // isError,
-    // isLoading,
-    // refetch,
-    // error,
-  } = useQuery({
-    queryKey: ["user", itemsPerPage, currentPage, debouncedSearch],
-    queryFn: async () => {
-      return fetch(`${env.NEXT_PUBLIC_API_URL}user/list`, {
-        body: JSON.stringify({
-          pageSize: itemsPerPage,
-          pageIndex: currentPage,
-          search: searchValue,
-          sortDescriptors: [
-            {
-              columnName: "firstName",
-              sortAscending: true,
-            },
-          ],
-        }),
-        headers: {
-          Authorization: "Bearer " + session!.user.accessToken,
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      })
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          }
-          throw new Error(`${res.status}`);
-        })
-        .then((json) => json.items);
-    },
-    enabled: !!session,
-  });
+  // const {
+  //   data: allUsers,
+  //   // isError,
+  //   // isLoading,
+  //   // refetch,
+  //   // error,
+  // } = useQuery({
+  //   queryKey: ["user", itemsPerPage, currentPage, debouncedSearch],
+  //   queryFn: async () => {
+  //     return fetch(`${env.NEXT_PUBLIC_API_URL}user/list`, {
+  //       body: JSON.stringify({
+  //         pageSize: itemsPerPage,
+  //         pageIndex: currentPage,
+  //         search: searchValue,
+  //         sortDescriptors: [
+  //           {
+  //             columnName: "firstName",
+  //             sortAscending: true,
+  //           },
+  //         ],
+  //       }),
+  //       headers: {
+  //         Authorization: "Bearer " + session!.user.accessToken,
+  //         "Content-Type": "application/json",
+  //       },
+  //       method: "POST",
+  //     })
+  //       .then((res) => {
+  //         if (res.ok) {
+  //           return res.json();
+  //         }
+  //         throw new Error(`${res.status}`);
+  //       })
+  //       .then((json) => json.items);
+  //   },
+  //   enabled: !!session,
+  // });
 
-  const usersWithoutOwner =
-    allUsers && allUsers.filter((user: User) => user.id !== owner);
+  const pageSize = 5;
+
+  const {
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    data,
+    status,
+    refetch,
+  } = useGetUsers(debouncedSearch, pageSize);
+
+  const intObserver = useRef<IntersectionObserver | null>(null);
+
+  const lastUserRef = useCallback(
+    (user: HTMLLIElement) => {
+      if (isFetchingNextPage) return;
+      if (intObserver.current) intObserver.current.disconnect();
+
+      intObserver.current = new IntersectionObserver((users) => {
+        if (users[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+      if (user) intObserver.current.observe(user);
+    },
+    [isFetchingNextPage, fetchNextPage, hasNextPage]
+  );
+
+  const users =
+    data &&
+    data.pages?.flatMap(({ items }) =>
+      items.map((user) => (
+        <UsersListItem
+          ref={lastUserRef}
+          key={user.id}
+          user={user}
+          onCheckboxChange={onCheckboxChange}
+          checked={isChecked(user)}
+        />
+      ))
+    );
 
   return (
     <ShareBudgetWrapperStyled>
       <ParagraphStyled>Invite existing members</ParagraphStyled>
       <SearchInput placeholder="Search" />
       <UsersListStyled>
-        {usersWithoutOwner &&
-          usersWithoutOwner.map((user: User) => (
-            <UsersListItem
-              key={user.id}
-              user={user}
-              onCheckboxChange={onCheckboxChange}
-              checked={isChecked(user)}
-            />
-          ))}
+        {users}
+
         {/* {mockUsers.map((user: User) => (
           <UsersListItem
             key={user.id}
---
+++ apps/web/lib/hooks/useGetUsers.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { env } from "env.mjs";
+import { useSession } from "next-auth/react";
+
+export type GetUsersListType = {
+  pageSize: number;
+  pageParam: number;
+  searchValue: string;
+  token: string | undefined;
+};
+
+export type User = {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  createdTimestamp: number;
+  createdVia: string;
+  avatar: string;
+};
+
+export type ItemType = {
+  items: User[];
+  totalCount: number;
+};
+
+const URL = `${env.NEXT_PUBLIC_API_URL}user/list`;
+
+export const getUsersList = async ({
+  pageSize,
+  pageParam,
+  searchValue,
+  token,
+}: GetUsersListType): Promise<ItemType> => {
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "*/*",
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      pageSize: pageSize,
+      pageIndex: pageParam,
+      search: searchValue,
+      sortDescriptors: [
+        {
+          columnName: "firstName",
+          sortAscending: true,
+        },
+      ],
+    }),
+  };
+
+  return fetch(URL, options).then((res) => res.ok && res.json());
+};
+
+export const useGetUsers = (searchValue: string, pageSize: number) => {
+  const { data: sessionData } = useSession();
+  const token = sessionData?.user.accessToken;
+
+  return useInfiniteQuery({
+    queryKey: ["budgetsList", { searchValue }],
+    queryFn: async ({ pageParam = 1 }): Promise<ItemType> => {
+      return getUsersList({
+        pageSize,
+        pageParam,
+        searchValue,
+        token,
+      });
+    },
+    getNextPageParam: (lastPage, allPages) => {
+      console.log(
+        lastPage && lastPage.items!.length ? allPages.length + 1 : undefined
+      );
+      return lastPage && lastPage.items!.length
+        ? allPages.length + 1
+        : undefined;
+    },
+    enabled: !!sessionData,
+  });
+};
