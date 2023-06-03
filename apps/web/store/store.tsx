acdf1febd612bce3221bb65526b48d0f3a65499d
Xewol <bartekdeluga0702@gmail.com>
2023-06-03 14:53:10 +0200

    feat: new store

--- apps/web/store/store.tsx
+++ apps/web/store/store.tsx
@@ -1,7 +1,9 @@
 import { atom } from "jotai";
+import { CategoryType } from "ui";
 
 export type Language = "en" | "pl" | "fr";
 
 export const categoryFilterAtom = atom<string[]>([]);
 export const languageAtom = atom<Language>("en");
 export const mobileMenuAtom = atom<boolean>(false);
+export const budgetCategories = atom<CategoryType[]>([]);
