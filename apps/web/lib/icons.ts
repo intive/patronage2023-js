8e57157d5c476362e5e47b3eacd9f975b4a31cc6
Xewol <bartekdeluga0702@gmail.com>
2023-06-05 09:24:11 +0200

    refactor: moved to separate file

---
+++ apps/web/lib/icons.ts
@@ -0,0 +1,13 @@
+import { IconType } from "ui";
+
+export const icons: IconType[] = [
+  "savings",
+  "directions_car",
+  "payments",
+  "subscriptions",
+  "shopping_cart",
+  "home",
+  "wallet",
+  "error",
+  "help",
+];
