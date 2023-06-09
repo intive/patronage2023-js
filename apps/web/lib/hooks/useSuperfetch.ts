4cd1906f928ab194e8a4e23fa5d34c69c24794f8
Daniel Chmur <daniel.chmur@gmail.com>
2023-06-09 16:21:42 +0200

    feat: export incomes/expenses by email #P23-363 (#151)

--- apps/web/lib/hooks/useSuperfetch.ts
+++ apps/web/lib/hooks/useSuperfetch.ts
@@ -56,9 +56,13 @@ export default function useSuperfetch() {
       } else if (res.status === 403) {
         console.log(t(dict.status403));
       } else if (res.status === 404) {
-        console.log(t(dict.status404));
+        showToast({ variant: "error", message: t(dict.status404) });
       } else if (res.status === 500) {
         console.log(t(dict.status500));
+        showToast({
+          variant: "error",
+          message: t(dict.status500),
+        });
       } else {
         showToast({
           variant: "error",
